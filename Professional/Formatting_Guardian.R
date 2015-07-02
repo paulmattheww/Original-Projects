3de5babc3c923c517a36d38cbf4692f48ecb12f9
Paul Matthew Washburn <paulmattheww@gmail.com>
2015-07-02 14:24:09 -0500

    Update Formatting_Guardian.R

--- Professional/Formatting_Guardian.R
+++ Professional/Formatting_Guardian.R
@@ -4,7 +4,7 @@
   # Delete redundant lines (eg. same person listed multiple times)
 
 # Set working directory
-setwd("C:/Users/pwashburn/Downloads")
+setwd("C:/Users/pwashburn/Desktop/Input")
 
 # Read in data
 Guardian<-read.csv("Guardian_Suntrup_06012015-06282015_Export.csv",
@@ -32,6 +32,9 @@ for(i in col)
     Guardian$Fill.6<- NA
     Guardian$Fill.7<- NA
     Guardian$Fill.8<- NA
+    Guardian$Fill.9<- NA
+    Guardian$Fill.10<- NA
+    Guardian$Fill.11<- NA
 Guardian$Fill.1<-ifelse(is.na(Guardian$Fill.1)," "," ")
 Guardian$Fill.2<-ifelse(is.na(Guardian$Fill.2)," "," ")
 Guardian$Fill.3<-ifelse(is.na(Guardian$Fill.3)," "," ")
@@ -40,6 +43,9 @@ Guardian$Fill.5<-ifelse(is.na(Guardian$Fill.5)," "," ")
 Guardian$Fill.6<-ifelse(is.na(Guardian$Fill.6)," "," ")
 Guardian$Fill.7<-ifelse(is.na(Guardian$Fill.7)," "," ")
 Guardian$Fill.8<-ifelse(is.na(Guardian$Fill.8)," "," ")
+Guardian$Fill.9<-ifelse(is.na(Guardian$Fill.6)," "," ")
+Guardian$Fill.10<-ifelse(is.na(Guardian$Fill.7)," "," ")
+Guardian$Fill.11<-ifelse(is.na(Guardian$Fill.8)," "," ")
 
 # Add gender to Relationship per Guardian specifications
 Guardian$Relationship..No.Codes.<-ifelse(Guardian$Relationship..No.Codes.=="EMPLOYEE","M",
@@ -54,50 +60,60 @@ Guardian$City<-gsub("\\.|/|\\-","",Guardian$City)
 # Hours per week get rid of NAs
 Guardian$Hours.Per.Week<-ifelse(!is.na(Guardian$Hours.Per.Week),Guardian$Hours.Per.Week,"")
 
-#### BELOW STILL NEEDS WORK
-  # Transform Reason.for.Change column into Guardian codes; 
-  # LA=Leave of Absence, MD=Member Death, TE=Terminated, AD=Adoption, BR=Birth, MR=Marriage, DI=Divorce, OT=Other, CB=Cobra, ST=State
+# Transform Reason.for.Change column into Guardian codes; 
+# LA=Leave of Absence, MD=Member Death, TE=Terminated, AD=Adoption, BR=Birth, MR=Marriage, DI=Divorce, OT=Other, CB=Cobra, ST=State
 Guardian$Reason.for.Change<-ifelse(Guardian$Reason.for.Change=="TERMINATION","TE",
                                    ifelse(Guardian$Reason.for.Change=="BIRTH","BR",
                                           ifelse(Guardian$Reason.for.Change=="MARRIAGE","MR",
                                                  ifelse(Guardian$Reason.for.Change=="DIVORCE","DI",
                                                         ifelse(Guardian$Reason.for.Change=="COBRA","CO","OT")))))
 
+# Add columns necessary
+dat<-Guardian$Coverage.Effective.Date
+dat<-as.character(dat)
+type<-Guardian$Benefit.Plan.Type
+
+Guardian$Dental.Elected<-ifelse(type=="DENTAL","Y","N")
+Guardian$Dental.Eff.Date<-ifelse(Guardian$Dental.Elected=="Y",dat," ")
+
+Guardian$Basic.Employee.Life.Elected<-ifelse(type=="BASIC EMPLOYEE LIFE","Y","N")
+Guardian$Basic.Life.Eff.Date<-ifelse(Guardian$Basic.Employee.Life.Elected=="Y",dat," ")
+
+Guardian$Voluntary.Child.Life.Elected<-ifelse(type=="VOLUNTARY CHILD LIFE","Y","N")
+Guardian$Vol.Child.Life.Eff.Date<-ifelse(Guardian$Voluntary.Child.Life.Elected=="Y",dat," ")
+
 # Change Time.Status..No.Codes. column A=Active, T=Terminated, R=Retired
 Guardian$Time.Status..No.Codes.<-ifelse(Guardian$Time.Status..No.Codes.=="FULL TIME SALARY","A",
        ifelse(Guardian$Time.Status..No.Codes.=="PART TIME","A",
               ifelse(Guardian$Time.Status..No.Codes.=="TERMINATED","T","R")))
 
-# Drop all columns that are not necessary and arrange by Guardian standards
-  # This part is good only through Termination Date; columns got shuffled for some reason
+
+# Create new data frame from Guardian object
+# Drop all columns that are not necessary 
 GuardianDf<-data.frame(Guardian)
 keeps<-names(GuardianDf) %in% c("Employee.SSN","Last.Name","First.Name","Middle.Initial","Relationship..No.Codes.",
                                 "Date.of.Birth","Gender","Marital.Status","Fill.1","Fill.2","Home.Address.1","City","State",
                                 "Zip","Work.Email","Work.Phone","Hire.Date","Hours.Per.Week","Employment.Status..No.Codes.",
-                                "Termination.Date","Reason.for.Change","Fill.3","Salary","Location","Tobacco.User..No.Codes.",
-                                "Fill.4","Fill.5","Fill.6","Fill.7","Fill.8","Benefit.Plan.Type","Coverage.Effective.Date",
-                                "Benefit.Class.Name")     ##ARE THERE ANYMORE WE NEED TO KEEP?
+                                "Termination.Date","Reason.for.Change","Fill.3","Salary","Fill.9","Fill.10","Location","Tobacco.User..No.Codes.",
+                                "Fill.4","Fill.5","Fill.6","Fill.7","Fill.8","Dental.Elected","Dental.Eff.Date","Fill.11",
+                                "Basic.Employee.Life.Elected","Basic.Life.Eff.Date","Voluntary.Child.Life.Elected",
+                                "Vol.Child.Life.Eff.Date","Benefit.Plan.Type","Coverage.Effective.Date","Benefit.Class.Name")      ##ARE THERE ANYMORE WE NEED TO KEEP?
 GuardianDf<-GuardianDf[keeps]
+
+# Put columns in same order as the Guardian spreadsheet
+orderColumns <- c("Employee.SSN","Last.Name","First.Name","Middle.Initial","Relationship..No.Codes.",
+                  "Date.of.Birth","Gender","Marital.Status","Fill.1","Fill.2","Home.Address.1","City","State",
+                  "Zip","Work.Email","Work.Phone","Hire.Date","Hours.Per.Week","Employment.Status..No.Codes.",
+                  "Termination.Date","Reason.for.Change","Fill.3","Salary","Fill.9","Fill.10","Location","Tobacco.User..No.Codes.",
+                  "Fill.4","Fill.5","Fill.6","Fill.7","Fill.8","Dental.Elected","Dental.Eff.Date","Fill.11",
+                  "Basic.Employee.Life.Elected","Basic.Life.Eff.Date","Voluntary.Child.Life.Elected",
+                  "Vol.Child.Life.Eff.Date","Benefit.Plan.Type","Coverage.Effective.Date","Benefit.Class.Name") 
+GuardianDf <- GuardianDf[, orderColumns]
 
 # Change Employment.Status..No.Codes. to format that Guardian wants 
   # A=Active; T=Terminated= R=Retired
 GuardianDf$Employment.Status..No.Codes.<- ifelse(GuardianDf$Employment.Status..No.Codes.=="ACTIVE","A",
        ifelse(GuardianDf$Employment.Status..No.Codes.=="RETIRED","R","T"))
-
-# Create columns that turn the type of policy into a factor-like Y or N field per Guardian's requirements
-  # Add Effective Date after each policy
-dat<-GuardianDf$Coverage.Effective.Date
-dat<-as.character(dat)
-type<-GuardianDf$Benefit.Plan.Type
-
-GuardianDf$Dental.Elected<-ifelse(type=="DENTAL","Y","N")
-GuardianDf$Dental.Eff.Date<-ifelse(GuardianDf$Dental.Elected=="Y",dat," ")
-
-GuardianDf$Basic.Employee.Life.Elected<-ifelse(GuardianDf$Benefit.Plan.Type=="BASIC EMPLOYEE LIFE","Y","N")
-GuardianDf$Basic.Life.Eff.Date<-ifelse(GuardianDf$Basic.Employee.Life.Elected=="Y",dat," ")
-
-GuardianDf$Voluntary.Child.Life.Elected<-ifelse(GuardianDf$Benefit.Plan.Type=="VOLUNTARY CHILD LIFE","Y","N")
-GuardianDf$Vol.Child.Life.Eff.Date<-ifelse(GuardianDf$Voluntary.Child.Life.Elected=="Y",dat," ")
 
 # Marital status codes don't matter for Guardian
 dat<-GuardianDf$Marital.Status
@@ -109,13 +125,21 @@ GuardianDf$Marital.Status<-ifelse(is.na(GuardianDf$Marital.Status)," "," ")
 GuardianDf$Work.Phone<-ifelse(is.na(GuardianDf$Work.Phone)," "," ")
 
 
-#### View(GuardianDf,"TEST")
-#### TURN PLAN TYPES INTO COLUMNS --> to be continued
+
+# View(GuardianDf,"TEST")
+
+
+
 
 # Write to CSV
-write.csv(GuardianDf,file="guardianOutput.csv")
-#library(xlsx)
-#write.xlsx(GuardianDf,"Guardian_ChgReport_Output.xlsx")
+print("Writing file to CSV.")
+write.csv(GuardianDf,file="OUTPUT-GUARDIAN-CHANGEREPORT.csv")
+
+
+
+
+
+
 
 
 #### STILL NEED TO CONSOLIDATE ROWS IF THE SAME NAME APPEARS MORE THAN ONCE WITHOUT DEPENDENTS
