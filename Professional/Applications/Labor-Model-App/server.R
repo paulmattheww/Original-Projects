6eb75dede4576da2d8cd31d4ed7e1b016dfcb848
Paul Matthew Washburn <paulmattheww@gmail.com>
2015-11-05 09:06:01 -0600

    Update server.R

--- Professional/Applications/Labor-Model-App/server.R
+++ Professional/Applications/Labor-Model-App/server.R
@@ -1,6 +1,7 @@
 # server.R
 library(shiny)
 library(caret)
+library(ggplot2)
 
 
 myData <- read.csv("stl_labor_model_data.csv", header=TRUE)
@@ -8,6 +9,23 @@ nightModelStl <- lm(INFERRED.NIGHT.EMPLOYEES ~ TOTAL.STL.CASES, data=myData)
 nightInterceptStl <- summary(nightModelStl)$coefficients[1]
 nightSlopeCasesStl <- summary(nightModelStl)$coefficients[2]
 
+actual <- data.frame(myData$NUMBER.OF.EMPLOYEES)
+actual$TYPE <- 'Actual'
+names(actual) <- c('Number.Night.Employees', 'Type')
+inferred <- data.frame(myData$INFERRED.NIGHT.EMPLOYEES)
+inferred$TYPE <- 'Inferred'
+names(inferred) <- c('Number.Night.Employees', 'Type')
+forHist <- rbind(inferred, actual)
+
+g <- ggplot(data=forHist, aes(Number.Night.Employees, fill=Type))
+nightDist <- function(input) {
+  renderPlot({
+    g + geom_density(alpha=0.2) + 
+      ggtitle(expression(atop('Actual Number of Night Employees per Evening vs. Inferred Number of Employees (STL)',
+                              atop(italic('Based on a 10 hour day'), "")))) +
+      theme(legend.position='bottom') + geom_vline(xintercept=input, colour='red')
+  })
+}
 
 shinyServer(
   function(input, output) {
@@ -19,11 +37,17 @@ shinyServer(
     prediction <- reactive({ round(nightInterceptStl + input$Cases * nightSlopeCasesStl, 1) 
       })
     
-    output$Predicted.Number <- renderText({ paste('Forecasted Number of Employees is', prediction()) 
+    output$Predicted.Number <- renderText({ paste('Shipping Employees Needed:', prediction()) 
       })
     
-    output$text2 <- renderText({
-      'This forecast does not account for special projects. Add in the number of hours needed for special projects.'
+    output$plot1 <- renderPlot({
+      g + geom_density(alpha=0.2) + 
+        ggtitle(expression(atop('Actual Number of Night Employees per Evening vs. Inferred Number of Employees (STL)',
+                                atop(italic('Based on a 10 hour day'), "")))) +
+        theme(legend.position='bottom') + geom_vline(xintercept=prediction(), colour='red')
     })
+      
+      #nightDist(prediction())
+    
 })
     
