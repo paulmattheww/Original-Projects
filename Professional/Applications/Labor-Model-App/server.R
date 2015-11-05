3fc25303c4c674e924d51176b9f1f4ce5af7da69
Paul Matthew Washburn <paulmattheww@gmail.com>
2015-11-05 10:14:47 -0600

    Update server.R

--- Professional/Applications/Labor-Model-App/server.R
+++ Professional/Applications/Labor-Model-App/server.R
@@ -2,6 +2,7 @@
 library(shiny)
 library(caret)
 library(ggplot2)
+library(gridExtra)
 
 
 myData <- read.csv("stl_labor_model_data.csv", header=TRUE)
@@ -18,14 +19,10 @@ names(inferred) <- c('Number.Night.Employees', 'Type')
 forHist <- rbind(inferred, actual)
 
 g <- ggplot(data=forHist, aes(Number.Night.Employees, fill=Type))
-nightDist <- function(input) {
-  renderPlot({
-    g + geom_density(alpha=0.2) + 
-      ggtitle(expression(atop('Actual Number of Night Employees per Evening vs. Inferred Number of Employees (STL)',
-                              atop(italic('Based on a 10 hour day'), "")))) +
-      theme(legend.position='bottom') + geom_vline(xintercept=input, colour='red')
-  })
-}
+p <- ggplot(data=myData, aes(TOTAL.STL.CASES))
+
+dayShift <- 8
+nightShift <- 10
 
 shinyServer(
   function(input, output) {
@@ -37,16 +34,27 @@ shinyServer(
     prediction <- reactive({ round(nightInterceptStl + input$Cases * nightSlopeCasesStl, 1) 
       })
     
-    output$Predicted.Number <- renderText({ paste('Shipping Employees Needed:', prediction()) 
+    goodPrediction <- reactive({ round(prediction() + (input$SpecialHours / nightShift), 1)
+      })
+    
+    output$Predicted.Number <- renderText({ paste('Shipping Employees Needed:', goodPrediction()) 
       })
     
     output$plot1 <- renderPlot({
-      g + geom_density(alpha=0.2) + 
+      g + geom_density(alpha=0.4) + 
         ggtitle(expression(atop('Employees per Evening vs. Inferred Number of Employees (STL)',
-                                atop(italic('Based on a 10 hour day'), "")))) +
-        theme(legend.position='bottom') + geom_vline(xintercept=prediction(), colour='red')
+                                atop(italic('Based on 2015 data Jan - Sept'), "")))) +
+        theme(legend.position='bottom') + geom_vline(xintercept=goodPrediction(), colour='red') +
+        labs(x='Number of Night Shift Employees') + geom_rug()
     })
       
+    output$plot2 <- renderPlot({
+      p + geom_density(alpha=0.4, fill='green') + 
+        ggtitle(expression(atop('Disitrubtion of Daily Cases (STL)',
+                                atop(italic('Based on a 10 hour day'), "")))) +
+        theme(legend.position='bottom') + geom_vline(xintercept=input$Cases, colour='red') +
+        labs(x='Total Cases Produced (Daily)') + geom_rug()
+    })
     
 })
     
