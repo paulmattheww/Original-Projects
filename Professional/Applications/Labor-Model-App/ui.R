c9dc19b18dba70d8d62366d1345393fdaee48889
Paul Matthew Washburn <paulmattheww@gmail.com>
2015-11-05 09:14:49 -0600

    Update ui.R

--- Professional/Applications/Labor-Model-App/ui.R
+++ Professional/Applications/Labor-Model-App/ui.R
@@ -3,23 +3,23 @@ library(shiny)
 shinyUI(fluidPage(
   titlePanel("Number of Employees Needed, Shipping St. Louis"),
   
-  sidebarLayout(
-    sidebarPanel(
-      helpText("Select the number of cases."),
-      sliderInput("Cases",
-                  "NUMBER OF CASES:",
-                  min=0,
-                  max=30000,
-                  value=13253)
-      ),
-    
-    mainPanel(
-      plotOutput("plot"))
-  ),
-  
   mainPanel(
+    helpText("Select the number of cases."),
+    
+    sliderInput("Cases",
+                "NUMBER OF CASES:",
+                min=0,
+                max=30000,
+                value=13253),
+    
+    h2(textOutput('Predicted.Number')),
+    
+    plotOutput('plot1'),
+    
     textOutput('text1'),
-    textOutput('Predicted.Number'),
-    textOutput('text2')
+    
+    p('This forecast does not account for special projects. 
+      Add in the number of hours needed for special projects.')
+    
   )
 ))
