11e8bfc5a34e5cc36191ad2accedc27449cf63b4
Paul Matthew Washburn <paulmattheww@gmail.com>
2015-11-09 16:59:02 -0600

    Update server.R

--- Professional/Applications/Labor-Model-App/server.R
+++ Professional/Applications/Labor-Model-App/server.R
@@ -3,6 +3,10 @@ library(shiny)
 library(caret)
 library(ggplot2)
 library(gridExtra)
+library(ggthemes)
+library(ggExtra)
+library(dplyr)
+library(lubridate)
 
 
 myData <- read.csv("stl_labor_model_data.csv", header=TRUE)
@@ -20,6 +24,7 @@ forHist <- rbind(inferred, actual)
 
 g <- ggplot(data=forHist, aes(Number.Night.Employees, fill=Type))
 p <- ggplot(data=myData, aes(TOTAL.STL.CASES))
+x <- ggplot(data=myData, aes(x=TOTAL.STL.CASES, y=INFERRED.NIGHT.EMPLOYEES))
 
 dayShift <- 8
 nightShift <- 10
@@ -43,16 +48,27 @@ shinyServer(
         ggtitle(expression(atop('Employees per Evening (STL)',
                                 atop(italic('Based on a 10 hour day'), "")))) +
         theme(legend.position='bottom') + geom_vline(xintercept=goodPrediction(), colour='red') +
-        labs(x='Number of Night Shift Employees') + geom_rug()
-    })
+        labs(x='Number of Night Shift Employees') + geom_rug(aes(colour=Type)) + 
+        theme_pander()
+    }, height=250)
       
     output$plot2 <- renderPlot({
       p + geom_density(alpha=0.4, fill='green') + 
         ggtitle(expression(atop('Distribution of Daily Cases (STL)',
                                 atop(italic('Based on 2015 data Jan - Sept'), "")))) +
         theme(legend.position='bottom') + geom_vline(xintercept=input$Cases, colour='red') +
-        labs(x='Total Cases Produced (Daily)') + geom_rug()
-    })
+        labs(x='Total Cases Produced (Daily)') + geom_rug() +
+        theme_pander() 
+    }, height=250)
+    
+    output$plot3 <- renderPlot({
+      x + geom_point() + 
+        geom_smooth(method="lm", se=T, colour='black') +
+        labs(title="Number of Employees as a funciton of Total Cases (STL)",
+             x="Total Cases STL", y="Number of Employees (Inferred from Hours) STL") +
+        theme_pander() + geom_vline(xintercept=input$Cases, colour='blue') +
+        geom_hline(yintercept=goodPrediction(), colour='blue') + geom_rug()
+    }, height=400)
     
     CPMH <- reactive({
       round(input$Cases / (round(goodPrediction(), 0) * 10), 2)
@@ -60,6 +76,26 @@ shinyServer(
     
     output$cpmhText <- renderText({
       paste('If all', goodPrediction(), 'employees work a 10 hour day, then we will achieve a CPMH of', CPMH())
+    })
+    
+    monthAvg <- reactive({
+      filter(myData, MONTH == input$Month)
+    })
+    
+    avgWorkersMonth <- reactive({
+      round(mean(monthAvg$INFERRED.NIGHT.EMPLOYEES), 1)
+    })
+    
+    maxDay <- reactive({
+      round(max(monthAvg$INFERRED.NIGHT.EMPLOYEES), 1)
+    })
+    
+    month <- reactive({
+      month.name[input$Month]
+    })
+    
+    output$avgWorkersMonth <- renderText({
+      paste('For', month(), 'the average shipping employees needed was')
     })
     
 })
