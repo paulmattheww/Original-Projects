1dda88292ae6343ed67403ef08f3849f6fbcdb71
Paul Matthew Washburn <paulmattheww@gmail.com>
2016-01-08 11:29:05 -0600

    Update CombineDailyReportFiles.R

--- Professional/Utility_Scripts/CombineDailyReportFiles.R
+++ Professional/Utility_Scripts/CombineDailyReportFiles.R
@@ -1,6 +1,7 @@
 dir <- "N:/Daily Report/2015/SEPT"
 unite = function(dir) {
   library(XLConnect)
+  library(stringr)
   setwd(dir)
   file_list = list.files(path=dir, pattern="(^[~$])*\\.xlsx$",
                          full.names=TRUE)
@@ -9,11 +10,13 @@ unite = function(dir) {
     production = readWorksheetFromFile(file_list[i], sheet=1,
                                         startCol=4, endCol=5,
                                         startRow=4, endRow=31)
+    production$Date = as.character(strptime(str_extract(as.character(file_list[i]), "(\\d+)-(\\d+)"), "%m-%d"))
     nightlyHours = readWorksheetFromFile(file_list[i], sheet=1,
                                           startCol=4, endCol=5,
                                           startRow=33, endRow=53)
-    names(production) = c('Metric', 'Value')
-    names(nightlyHours) = c('Metric', 'Value')
+    nightlyHours$Date = as.character(strptime(str_extract(as.character(file_list[i]), "(\\d+)-(\\d+)"), "%m-%d"))
+    names(production) = c('Metric', 'Value', 'Date')
+    names(nightlyHours) = c('Metric', 'Value', 'Date')
     temp = data.frame(rbind(production, nightlyHours))
     output = if(!exists("output")){
       output = temp
