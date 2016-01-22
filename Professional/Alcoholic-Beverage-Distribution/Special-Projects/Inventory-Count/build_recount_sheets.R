c016e844cb5e1bf601a3f59f8b2e6f8a52513556
Paul Matthew Washburn <paulmattheww@gmail.com>
2016-01-22 06:29:49 -0600

    Update build_recount_sheets.R

--- Professional/Alcoholic-Beverage-Distribution/Special-Projects/Inventory-Count/build_recount_sheets.R
+++ Professional/Alcoholic-Beverage-Distribution/Special-Projects/Inventory-Count/build_recount_sheets.R
@@ -1,28 +1,42 @@
-print('Filter by wave first since this will be iterated by wave; save wave number as object')
+print('Building recount sheets for KC')
+
+print('Specifying wave')
 WAVE = 1
-setwd("C:/Users/pmwash/Desktop/Projects/Inventory Count 2016")
+print('Change to M:/Inv2016/Recount Sheets')
+setwd("M:/Inv2016/Recount Sheets")
+print('Reading in Analysis & DataEntry tab to pass as arguments to build_recount_sheets() function')
 analysis = read.xlsx('adapt_to_create_recount_sheets.xlsx', sheetName='Analysis')
 entry = read.xlsx('adapt_to_create_recount_sheets.xlsx', sheetName='DataEntry')
 
 build_recount_sheets = function(analysis, entry, wave) {
   print('Did you change the wave number prior to running this script?')
-  library(xlsx)
+  library(XLConnect)
   library(dplyr)
-  analysis$Case.Diff.Incl.Btls = round(analysis$Difference.Ttl.Btls / analysis$CaseConv, 2)
-  analysis = analysis[,c('item_number', 'description', 'Difference.CASE', 'Difference.COST', 'Wave', 'Case.Diff.Incl.Btls')]
-  recount_items = analysis %>% filter(Difference.COST >= 300 | Case.Diff.Incl.Btls >= 3)
-  key_recount_items = recount_items[,c('item_number', 'description')]
+  print('Creating absolute value of case difference, including bottles (using QPC)')
+  analysis$Case.Diff.Incl.Btls = round(abs(analysis$Difference.Ttl.Btls) / analysis$QPC, 2)
+  analysis = analysis[,c('Item..', 'description', 'Difference.CASE', 'Difference.COST', 'Wave', 'Case.Diff.Incl.Btls')]
+  print('Filtering where differences are >= 3 cases or $300')
+  recount_items = analysis %>% filter(abs(Difference.COST) >= 300 | Case.Diff.Incl.Btls >= 3)
+  key_recount_items = recount_items[,c('Item..', 'description')]
   names(key_recount_items) = c('Item..', 'Description')
+  print('Merging together analysis sheet filtered for 3cs/$300 with location entry sheet')
   recounts = merge(entry, key_recount_items, by='Item..')
-  recounts = recounts[,c('Item..', 'QPC', 'bottle_size', 'Desc', 'Vintage', 'Loc', 'CASE.COUNTED', 'BOTTLES.COUNTED')]
+  recounts = recounts[,c('Item..', 'QPC', 'bottle_size', 'Desc', 'Vintage', 'Loc', 'Case.Cnt.One', 'Btl.Cnt.One')]
+  print('Splitting the dataset by item number for printing')
   recounts_split = split(recounts, recounts$Item..)
-  setwd("C:/Users/pmwash/Desktop/R_Files/Data Output")
-  print('Did you make sure all of the sheets except the formatting template are gone?')
+  setwd("M:/Inv2016/Recount Sheets")
+  print('Printing separate items with locations to a single xlsm workbook')
   lapply(1:length(recounts_split), function(i) write.xlsx(recounts_split[[i]], 
                                                           file='recount_sheets.xlsm',
                                                           sheetName=paste0('Wave_', WAVE, '_Item_', i, '_ID_', names(recounts_split[i])), 
                                                           append=TRUE)) 
   print('Check to make sure that all files sheets were printed')
+  print('When done save as... the recount sheets by wave')
+  print('Items & their locations that need to be re-counted have been printed to recount_sheets.xlsm')
 }
 
 build_recount_sheets(analysis, entry, WAVE)
+
+
+
+
