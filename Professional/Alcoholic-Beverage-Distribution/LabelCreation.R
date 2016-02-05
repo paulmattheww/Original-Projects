907c57d9f3dd179e4924e8ee0a9188667b052603
Paul Matthew Washburn <paulmattheww@gmail.com>
2016-02-05 08:48:48 -0600

    Update LabelCreation.R

--- Professional/Alcoholic-Beverage-Distribution/LabelCreation.R
+++ Professional/Alcoholic-Beverage-Distribution/LabelCreation.R
@@ -1,44 +1,34 @@
-substrRight <- function(x, n){
-  substr(x, nchar(x)-n+1, nchar(x))
+print('Label Creation, Upstream from Wasp')
+
+
+
+print('BOTUPC file AS400')
+upc = read.csv('C:/Users/pmwash/Desktop/R_files/Data Input/BOTUPC.csv')
+
+print('Locations from Bob Kloeppinger')
+locsBob = read.csv('C:/Users/pmwash/Desktop/R_files/Data Input/locations_bob_labels.csv')
+
+print('Inventory as of today from High Jump')
+locations = read.csv('C:/Users/pmwash/Desktop/R_files/Data Input/all_inventory.csv')
+
+make_labels = function(upc, locsBob, locations) {
+  substrRight <- function(x, n){
+    substr(x, nchar(x)-n+1, nchar(x))
+  }
+  print('Columns in BOTUPC must be formatted as numbers with no decimals.')
+  options(scipen=999)
+  names(locations) <- c('Product.Id', 'Description', 'Location', 'QPC',
+                        'Bottle.Size', 'Total.Bottles', 'Cases', 'Bottles')
+  names(upc) = c('Product.Id', 'UPC')
+  output = merge(locations, upc, by='Product.Id', all=TRUE)
+  print('Locations succcessfully merged with bottle UPCs')
+  output = merge(output, locsBob, by='Location', all.y=TRUE)
+  print('Locations/UPC successfully merged with Label Locations')
+  output = output[,c('Product.Id', 'Location', 'Description', 'UPC')]
+  output$Last.3 = substrRight(output$UPC, 3)
+  write.csv(output, 'C:/Users/pmwash/Desktop/R_files/Data Output/locations_upcs_for_labels.csv')
+  print('Output file produced. Format Last.3 column and input into Wasp for label production')
 }
 
-
-
-# Merge Item.No, Location, and UPC for printing label locations
-setwd("C:/Users/pmwash/Desktop/R_files/Data Input")
-
-upc <- read.csv('BOTUPC.csv', header=TRUE) # this is a file in as400 that you will need to run
-names(upc) <- c('Product.Id', 'UPC')
-head(upc)
-
-locsBob <- read.csv('locations_bob_labels.csv', header=TRUE) # Bob will send a list of locations only
-locsBob <- locsBob[,c(1:2)]
-names(locsBob) <- c('Location', 'nanana')
-head(locsBob)
-
-locations <- read.csv("all_inventory.csv", header=TRUE) # this is today's inventory from hi jump 
-names(locations) <- c('Product.Id', 'Description', 'Location', 'QPC',
-                      'Bottle.Size', 'Total.Bottles', 'Cases', 'Bottles')
-locations <- locations[,c('Product.Id', 'Location', 'Description')]
-head(locations)
-
-
-forLabels <- merge(locations, upc, by='Product.Id')
-forLabels <- merge(forLabels, locsBob, by='Location')
-forLabels <- forLabels[,c('Product.Id', 'Location', 'UPC', 'Description')]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+make_labels(upc, locsBob, locations)
 
