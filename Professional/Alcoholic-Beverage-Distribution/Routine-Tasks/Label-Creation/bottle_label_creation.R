31864daad69dbf986d3586c639a190ea600e0232
Paul Matthew Washburn <paulmattheww@gmail.com>
2016-02-05 09:31:36 -0600

    Update and rename label_creation.R to bottle_label_creation.R

--- Professional/Alcoholic-Beverage-Distribution/Routine-Tasks/Label-Creation/bottle_label_creation.R
+++ Professional/Alcoholic-Beverage-Distribution/Routine-Tasks/Label-Creation/bottle_label_creation.R
@@ -5,7 +5,7 @@ print('Label Creation, Upstream from Wasp')
 print('BOTUPC file AS400')
 upc = read.csv('C:/Users/pmwash/Desktop/R_files/Data Input/BOTUPC.csv')
 
-print('Locations from Bob Kloeppinger')
+print('Locations from Bob Kloeppinger; put locations in one column and one column marked X')
 locsBob = read.csv('C:/Users/pmwash/Desktop/R_files/Data Input/locations_bob_labels.csv')
 
 print('Inventory as of today from High Jump')
@@ -15,7 +15,7 @@ make_labels = function(upc, locsBob, locations) {
   substrRight <- function(x, n){
     substr(x, nchar(x)-n+1, nchar(x))
   }
-  print('Columns in BOTUPC must be formatted as numbers with no decimals.')
+  print('Columns in BOTUPC must be formatted as numbers with no decimals prior to reading in data')
   options(scipen=999)
   names(locations) <- c('Product.Id', 'Description', 'Location', 'QPC',
                         'Bottle.Size', 'Total.Bottles', 'Cases', 'Bottles')
@@ -27,7 +27,9 @@ make_labels = function(upc, locsBob, locations) {
   output = output[,c('Product.Id', 'Location', 'Description', 'UPC')]
   output$Last.3 = substrRight(output$UPC, 3)
   write.csv(output, 'C:/Users/pmwash/Desktop/R_files/Data Output/locations_upcs_for_labels.csv')
-  print('Output file produced. Format Last.3 column and input into Wasp for label production')
+  print('Output file produced. Format Last.3 column/paste values, save as Excel 97-03 and input into Wasp for label production')
+  print('Verify in High Jump all is well by searching first by location, then drill into the item to check UPC')
+  print('Delete description field after you have checked')
 }
 
 make_labels(upc, locsBob, locations)
