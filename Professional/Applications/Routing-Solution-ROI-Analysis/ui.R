08e8d627839df2a2566615fb293187fc74cbb872
Paul Matthew Washburn <paulmattheww@gmail.com>
2016-03-22 14:50:23 -0500

    Update ui.R

--- Professional/Applications/Routing-Solution-ROI-Analysis/ui.R
+++ Professional/Applications/Routing-Solution-ROI-Analysis/ui.R
@@ -6,47 +6,56 @@ shinyUI(fluidPage(
   mainPanel(
     plotOutput('plot1'),
     
-    fluidRow(
-      sliderInput("fuel",
-                  "Savings in Fuel:",
-                  min=-0.01,
-                  max=0.15,
-                  value=0.03),
-      
-      sliderInput("driver",
-                  "Savings in Driver Compensation:",
-                  min=-0.01,
-                  max=0.15,
-                  value=0.02),
-      
-      sliderInput("truck",
-                  "Probability of 1 Truck Off Road by March 2017:",
-                  min=0.01,
-                  max=0.99,
-                  value=0.5),
-      
-      sliderInput("inflator",
-                  "Implementation Budget Overage:",
-                  min=1.0,
-                  max=2.0,
-                  value=1.0),
-      
-      numericInput("telematics",
-                   label="Telematics (Use $19,171 if simulating with Telematics):",
-                   value=0),
-      
-      numericInput("maintenance",
-                   label="Anticipated Savings on Maintenance:",
-                   value=0),
-      
-      numericInput("analyst",
-                   label="Anticipated Savings to Analyst's Time:",
-                   value=0),
-      
-      numericInput("router",
-                   label="Anticipated Savings to Router's Time:",
-                   value=0),
+    tabsetPanel(
+      tabPanel('Data', 
+                dataTableOutput('data')),
     
+      tabPanel('Parameters',
+        fluidRow(
+          numericInput("negotiations",
+                       label="Negotiate Annual Roadnet Fee w/o Telematics (Quote of $79,002/yr):",
+                       value=79002.12),
+          
+          sliderInput("fuel",
+                      "Annual % Savings in Fuel Consumption:",
+                      min=-0.01,
+                      max=0.15,
+                      value=0.03),
+          
+          sliderInput("driver",
+                      "Annual % Savings in Driver Compensation:",
+                      min=-0.01,
+                      max=0.15,
+                      value=0.02),
+          
+          sliderInput("truck",
+                      "Probability of 1 Truck Off Road by March 2017:",
+                      min=0.01,
+                      max=0.99,
+                      value=0.5),
+          
+          sliderInput("inflator",
+                      "Implementation & Consulting Cost Inflator:",
+                      min=1.0,
+                      max=2.0,
+                      value=1.0),
+          
+          numericInput("telematics",
+                       label="Annual Telematics Cost ($19,171):",
+                       value=0),
+          
+          numericInput("maintenance",
+                       label="Anticipated Savings on Maintenance ($/mo):",
+                       value=0),
+          
+          numericInput("analyst",
+                       label="Value of Having Vital Operational Data & Advanced Analytics ($/mo):",
+                       value=0),
+          
+          numericInput("router",
+                       label="Anticipated Savings to Router's Time ($/mo):",
+                       value=0))),
+        
 
     br()
 
