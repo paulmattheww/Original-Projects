a182ce757ec95617b080619dd6f261fb34317b37
Paul Matthew Washburn <paulmattheww@gmail.com>
2016-03-23 09:23:18 -0500

    Update ui.R

--- Professional/Applications/Routing-Solution-ROI-Analysis/ui.R
+++ Professional/Applications/Routing-Solution-ROI-Analysis/ui.R
@@ -10,9 +10,9 @@ shinyUI(fluidPage(
       tabPanel('Key Considerations', 
                h3('Key Considerations'),
                p('KC mileage data was unavailable at the time of data gather. 
-                  This increases uncertainty surrounding being capable of 
-                  combining routes that are underutilized by capacity and proximity alone. 
-                  It is still likely that overall capacity utilization will increase, even amongst crowded routes.'),
+                 This increases uncertainty surrounding being capable of 
+                 combining routes that are underutilized by capacity and proximity alone. 
+                 It is still likely that overall capacity utilization will increase, even amongst crowded routes.'),
                p('Rural routes are more difficult to combine, even on a daily basis, 
                  due to the large number of miles that they travel per day.'),
                p('While it will be more expensive to do Telematics, we are have considerably less 
@@ -30,70 +30,70 @@ shinyUI(fluidPage(
                  need to set up new SOPs for routers to maintain data, and establish expectations/responsibilities.')),
       
       tabPanel('Reactively Generated Data', 
-                dataTableOutput('data')),
-    
-      tabPanel('User-Defined Parameters',
-        fluidRow(
-          numericInput("negotiations",
-                       label="Negotiate Annual Roadnet Fee w/o Telematics (Quote of $79,002/yr):",
-                       value=79002.12),
-          
-          sliderInput("fuel",
-                      "Annual % Savings in Fuel Consumption:",
-                      min=-0.01,
-                      max=0.15,
-                      value=0.03),
-          
-          sliderInput("driver",
-                      "Annual % Savings in Driver Compensation:",
-                      min=-0.01,
-                      max=0.15,
-                      value=0.02),
-          
-          sliderInput("truck",
-                      "Probability of 1 Truck Off Road by March 2017:",
-                      min=0.01,
-                      max=0.99,
-                      value=0.5),
-          
-          sliderInput("inflator",
-                      "Implementation & Consulting Cost Inflator:",
-                      min=1.0,
-                      max=2.0,
-                      value=1.0),
-          
-          numericInput("telematics",
-                       label="Annual Telematics Cost ($19,171):",
-                       value=0),
-          
-          numericInput("maintenance",
-                       label="Anticipated Maintenance Savings ($/mo):",
-                       value=0),
-          
-          numericInput("analyst",
-                       label="Value of Organized Operational Data & Advanced Analytics ($/mo):",
-                       value=0),
-          
-          numericInput("router",
-                       label="Anticipated Savings -OR- Costs to Router's Time ($/mo):",
-                       value=0))),
+               dataTableOutput('data')),
       
-      tabPanel('Key Outputs',
+      tabPanel('User-Defined Parameters',
+               fluidRow(
+                 column(12, wellPanel(
+                   numericInput("negotiations",
+                                label="Negotiate Annual Roadnet Fee w/o Telematics (Quote of $79,002/yr):",
+                                value=79002.12),
+                   
+                   sliderInput("fuel",
+                               "Annual % Savings in Fuel Consumption:",
+                               min=-0.01,
+                               max=0.15,
+                               value=0.03),
+                   
+                   sliderInput("driver",
+                               "Annual % Savings in Driver Compensation:",
+                               min=-0.01,
+                               max=0.15,
+                               value=0.02),
+                   
+                   sliderInput("truck",
+                               "Probability of 1 Truck Off Road by March 2017:",
+                               min=0.01,
+                               max=0.99,
+                               value=0.5),
+                   
+                   sliderInput("inflator",
+                               "Implementation & Consulting Cost Inflator:",
+                               min=1.0,
+                               max=2.0,
+                               value=1.0),
+                   
+                   numericInput("telematics",
+                                label="Annual Telematics Cost ($19,171):",
+                                value=0),
+                   
+                   numericInput("maintenance",
+                                label="Anticipated Equipment Maintenance Savings ($/mo):",
+                                value=0),
+                   
+                   numericInput("analyst",
+                                label="Value of Organized Operational Data & Advanced Analytics ($/mo):",
+                                value=0),
+                   
+                   numericInput("router",
+                                label="Anticipated Savings -OR- Costs to Router's Time ($/mo):",
+                                value=0))))),
+      
+      tabPanel('Key Outputs', wellPanel(
                h3('Key Outputs'),
                br(),
                downloadButton('downloadData', 'Download Version'),
                br(),
                br(),
-               h4('Raw ROI at end of year 3:'), 
+               h4('Un-Discounted ROI at end of year 3:'), 
                p(textOutput('net_savings_year_3')),
                br(),
                h4('Months Til Positive Cash Flow:'), 
                p(textOutput('months_to_roi')),
-               br()
-               ),
-        
-
-    br()
-
-  )
-)))
+               br())
+      ),
+      
+      
+      br()
+      
+))))
