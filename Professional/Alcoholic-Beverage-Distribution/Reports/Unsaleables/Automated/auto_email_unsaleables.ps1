d24c1660d433e7379c7b75e48782504eeb7fd98b
Paul Matthew Washburn <paulmattheww@gmail.com>
2016-05-17 15:16:08 -0500

    Update auto_email_unsaleables.ps1

--- Professional/Alcoholic-Beverage-Distribution/Reports/Unsaleables/Automated/auto_email_unsaleables.ps1
+++ Professional/Alcoholic-Beverage-Distribution/Reports/Unsaleables/Automated/auto_email_unsaleables.ps1
@@ -7,7 +7,7 @@ $Year = $Date.Year
 
 $FileName = $Month + "_" + $Day + "_" + $Year + "_unsaleables_returns_dumps.xlsx"
 $FilePath = "N:\Operations Intelligence\Monthly Reports\Unsaleables\Data\" + $FileName
-$Folder = "/Operations%20Intelligence/Monthly%20Reports/Unsaleables/Data/" + $FileName
+$Folder = "/Operations%20Intelligence/Monthly%20Reports/Unsaleables/Data" #+ $FileName
 $PathTitle = "MTD Unsaleables, Returns & Dumps"
 
 $ClickablePath = "<a href=file://localhost/N:/"+$Folder+">Month-To-Date Unsaleables by Item, Supplier & Customer</a>"
@@ -22,8 +22,8 @@ $MessageParameters = @{
 
     SmtpServer = "outlook.majorbrands.com"
     From = "paul.washburn@majorbrands.com"
-    To = "jill.sites@majorbrands.com", "mitch.turner@majorbrands.com"
-    Cc = "dan.monks@majorbrands.com", "paul.washburn@majorbrands.com"
+    To = "paul.washburn@majorbrands.com"#"jill.sites@majorbrands.com", "mitch.turner@majorbrands.com"
+    #Cc = "dan.monks@majorbrands.com", "paul.washburn@majorbrands.com"
     
     Subject = "MTD Unsaleables for " + $Month + "/" + $Day + "/" + $Year
     Body = "<html>
@@ -37,18 +37,10 @@ $MessageParameters = @{
     <body>
         <h3>MTD Unsaleables by Item & Supplier</h3>
         <p>
-                This is the first test of a custom built automatic report generator.<br />
-                The mechanisms that generate this report are under development.<br /><br />
+                " + $ClickablePath + "<br /> <br />
                 
-                Please let me know how often you would like these numbers run <br />
-                and I will set it up to run at your convenience. <br /><br />
-                
-                Follow the path below to find MTD unsaleables by item & supplier.<br /><br />
-                
-                Find the summary here:  <br /> <br />" + $ClickablePath + "<br /> <br />
-                
-                If you plan on editing this report, then save the workbook to a<br />
-                your hard drive. This will preserve the source file for others. <br />
+                If you plan on editing this report, save the workbook to<br />
+                your hard drive. This will preserve the source file for others.<br />
                 If you download the file, it is recommended that you filter for your<br />
                 products/suppliers and delete the rest as the file is large.<br /><br />
                 
