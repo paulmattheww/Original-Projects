a69aef0380ab9f65d86ebba345cffae435dbfe9e
Paul Matthew Washburn <paulmattheww@gmail.com>
2016-05-17 15:18:30 -0500

    Create mtd_unsaleables.bat

---
+++ Professional/Alcoholic-Beverage-Distribution/Reports/Unsaleables/Automated/mtd_unsaleables.bat
@@ -0,0 +1,5 @@
+powershell C:\Users\pmwash\Desktop\PowerShell\Reports\Unsaleables\run_unsaleables.ps1
+timeout /t 15
+powershell C:\Users\pmwash\Desktop\PowerShell\Reports\Unsaleables\format_unsaleables.ps1
+timeout /t 15
+powershell C:\Users\pmwash\Desktop\PowerShell\Reports\Unsaleables\auto_email_unsaleables.ps1
