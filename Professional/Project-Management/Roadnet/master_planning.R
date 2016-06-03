411b8a6960f69a1160351f538b480051f36ab795
Paul Matthew Washburn <paulmattheww@gmail.com>
2016-06-03 15:47:46 -0500

    Update master_planning.R

--- Professional/Project-Management/Roadnet/master_planning.R
+++ Professional/Project-Management/Roadnet/master_planning.R
@@ -10,28 +10,115 @@ library(network)
 library(sna)
 library(ndtv)
 library(dplyr)
-
+library(grDevices)
 library(plotrix)
 
 
+refresh_data = function() {
+  edges = read.xlsx(file='C:/Users/pmwash/Desktop/Roadnet Implementation/Data/Planning Data/master_planning_roadnet.xlsx', 
+                    sheetName='edges', header=TRUE)
+  nodes = read.xlsx(file='C:/Users/pmwash/Desktop/Roadnet Implementation/Data/Planning Data/master_planning_roadnet.xlsx', 
+                    sheetName='nodes_w_types', header=TRUE)
+  tasks = read.xlsx(file='C:/Users/pmwash/Desktop/Roadnet Implementation/Data/Planning Data/master_planning_roadnet.xlsx',
+                    sheetName='nodes_w_types', header=TRUE)
+  tasks = filter(tasks, type == 'Task')
+  players = read.xlsx(file='C:/Users/pmwash/Desktop/Roadnet Implementation/Data/Planning Data/master_planning_roadnet.xlsx',
+                      sheetName='nodes_w_types', header=TRUE)
+  players = filter(players, type != 'Task')
+}
+# refresh_data()
 
-planning = read.xlsx(file='C:/Users/pmwash/Desktop/Roadnet Implementation/Data/Planning Data/master_planning_roadnet.xlsx', 
-          sheetName='roadnet_gantt', header=TRUE)
-edges = read.xlsx(file='C:/Users/pmwash/Desktop/Roadnet Implementation/Data/Planning Data/master_planning_roadnet.xlsx', 
-                     sheetName='edges', header=TRUE)
-nodes_w_types = read.xlsx(file='C:/Users/pmwash/Desktop/Roadnet Implementation/Data/Planning Data/master_planning_roadnet.xlsx', 
-                     sheetName='nodes_w_types', header=TRUE)
 
-planning$Start = as.character(as.POSIXct(planning$Start, '%Y/%m/%d'))
-planning$End = as.character(as.POSIXct(planning$End, '%Y/%m/%d'))
+# DETAILED TASK PLANNING
 
 
+events = c('TODAY')
+events_dates = c(as.character(Sys.Date()))
 
-# TIMELINE PLANNING
+
+head(tasks)
+dev.off()
+create_gantt = function(key, desc, start, end, done, events, event_dates){
+  roadnet_planner = as.gantt(key=key, description=desc, 
+                             start=start, end=end, done=done) #, # done) #, NeededBy) #, done, neededBy)
+  
+  x = plot(roadnet_planner, 
+           event.label=events,
+           event.time=events_dates,
+           time.lines.by="1 week",
+           main='Major Tasks Remaining in Roadnet',
+           col.done='green',
+           col.notdone='gray',
+           cex=1)
+  
+  print(x)
+}
+
+png('N:/2016 MB Projects/Roadnet/Planning/timeline.png', width = 1200, height = 800)
+create_gantt(key=tasks$key, desc=tasks$label, start=tasks$start, 
+             end=tasks$end, done=tasks$done, events=events, event_dates=event_dates)
+dev.off()
+
+
+
+
+
+
+
+
+# list of tasks and players
+
+order_players = players$label
+order_tasks = unique(tasks$label)
+
+
+tasks_ppl = merge(tasks, edges, by.x='nodes', by.y='to', all=TRUE)
+
+tasks_ppl = filter(tasks_ppl, done < 100)
+tasks_ppl = tasks_ppl[, c('label', 'initials', 'rel', 'end')]
+tasks_ppl$rel = factor(tasks_ppl$rel, levels=c('R', 'A', 'C', '_'))
+tasks_ppl$initials = factor(tasks_ppl$initials, levels=order_players)
+tasks_ppl$label = factor(tasks_ppl$label, levels=order_tasks)
+
+library(tidyr)
+tasks_ppl = spread(tasks_ppl, initials, rel, fill='_')
+tasks_ppl = arrange(tasks_ppl, end)
+row.names(tasks_ppl) = tasks_ppl$label
+tasks_ppl$label = NULL
+
+
+library(htmlTable)
+
+htmlTable(tasks_ppl)
+
+
+
+
+
+library(xtable)
+
+# print(xtable(tasks_ppl, caption='x', label=NULL))
+
+
+
+
+View(tasks_ppl)
+
+
+
+
+
+
+
+# HIGH LEVEL TIMELINE PLANNING
+planning = read.xlsx(file='C:/Users/pmwash/Desktop/Roadnet Implementation/Data/Planning Data/master_planning_roadnet.xlsx',
+                     sheetName='timeline_highlevel', header=TRUE)
+
+
 events = c('Sign Contract', 
            'Go Live!')
 events_dates = c('2016-05-12', 
-           '2016-09-01')
+                 '2016-09-01')
 
 
 roadnet_planner = as.gantt(planning$Key, planning$Description, 
@@ -43,8 +130,8 @@ plot(roadnet_planner,
      event.time=events_dates, 
      time.lines.by="1 week",
      main='Major Tasks Remaining in Roadnet',
-     col.done='green',
-     col.notdone='lightblue',
+     # col.done='green',
+     col.notdone='lightgreen',
      cex=1)
 
 
@@ -53,58 +140,246 @@ plot(roadnet_planner,
 
 # TASKS
 
+
+
+
+
 # select for department
 #nodes = filter(nodes, department == 'OPS' | department == '')
 
+refresh_data()
 
 nodes_w_types = create_nodes(nodes = nodes$nodes,
-               type = nodes$type,
-               label = nodes$label,
-               style = nodes$style, 
-               shape = nodes$shape,
-               #fillcolor = nodes$fillcolor,
-               cex.bold = nodes$fontbold
-               ) 
+                             type = nodes$type,
+                             label = nodes$label,
+                             style = nodes$style, 
+                             shape = nodes$shape,
+                             #fillcolor = nodes$fillcolor,
+                             cex.bold = nodes$fontbold
+) 
 
 edges = create_edges(from = edges$from,
-               to = edges$to,
-               rel = edges$rel,
-               width = edges$width,
-               cex.bold = edges$fontbold
-               )
+                     to = edges$to,
+                     rel = edges$rel,
+                     width = edges$width,
+                     cex.bold = edges$fontbold
+)
 
 
 
 
-output_graph = 
-  create_graph(nodes_df = nodes_w_types,
-               edges_df = edges,
-               graph_attrs = "layout = neato",
-               node_attrs = "fontname = Helvetica",
-               edge_attrs = "color = gray20"
-               )
+g_att = c('layout = neato', #neato dot twopi circo visNetwork
+          'overlap = FALSE', 
+          'outputorder = edgesfirst')
 
-render_graph(graph = output_graph, 
-             # output = 'forcedDirection')
-             output = 'visNetwork')  
-             #output = 'visNetwork')
+output_graph = create_graph(nodes_df = nodes_w_types,
+                            edges_df = edges,
+                            graph_attrs = g_att,
+                            node_attrs = "fontname = Helvetica",
+                            edge_attrs = "color = gray20"
+  )
 
-# ,
-#              node_attrs = "fontname = Helvetica",
-#              edge_attrs = c("color = black", "arrowsize = 2"))
+render_graph(graph = output_graph, width=1200, height=1500,
+             output = 'visNetwork')#, layout=dot)  
+
+
+
+
+
+
+
+# Sankey plot 
+
+edges = read.xlsx(file='C:/Users/pmwash/Desktop/Roadnet Implementation/Data/Planning Data/master_planning_roadnet.xlsx', 
+                  sheetName='edges', header=TRUE)
+
+order_players = players$node
+order_tasks = unique(tasks$label)
+df = edges
+
+
+sankey_df = function(df, colsource='source', coltarget='target', colvalue='value') {
+  library(dplyr)
+  
+  sankey.df = subset(df, select=c(colsource, coltarget, colvalue))
+  colnames(sankey.df) = c('source', 'target', 'value')
+  
+  sankey.df$source = factor(sankey.df$source, levels=order_players)
+  sankey.df$target = factor(sankey.df$target, levels=order_tasks)
+  sankey.df = arrange(sankey.df, source)
+  
+  sankey.df
+}
+
+df = sankey_df(df=edges, colsource='from', coltarget='tolabel', colvalue='value')
+names(df) = c('target', 'source', 'value')
+# df$source = factor(df$source, levels=order_players)
+# df$target = factor(df$target, levels=order_tasks)
+df = df %>% arrange(target)
+
+
+
+sankey_plot = function(df, title) {
+  library(rCharts)
+  
+  sankeyPlot <- rCharts$new()
+
+  #We also need to point to an HTML template page
+  sankeyPlot$setLib('http://timelyportfolio.github.io/rCharts_d3_sankey')
+
+  sankeyPlot$set(
+    data = df,
+    nodeWidth = 25,
+    nodePadding = 10,
+    layout = 32,
+    width = 900,
+    height = 1100,
+    labelFormat = ".1%",
+    title=title
+  )
+  
+  sankeyPlot
+  
+}
+
+#dev.off()
+s = sankey_plot(df, title='Team Member/Task Relationships')
+s
+
+library(caTools)
+library(png)
+
+dev.off()
+write.gif(s, 'test_plot.gif')
+
+
+
+
+
+
+head(edges)
+
+
+
+
+
+
+# Chord diagram
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 edge_count = count_edges(output_graph)
 
 
 edges = get_edges(graph_nodes_edges,
-          edge_attr = "rel",
-          match = "rel_a",
-          return_type = "list") #df vector
+                  edge_attr = "rel",
+                  match = "rel_a",
+                  return_type = "list") #df vector
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# library(grDevices)
+# library(plotrix)
+# 
+# d_format = '%Y/%m/%d'
+# 
+# planning = read.xlsx(file='C:/Users/pmwash/Desktop/Roadnet Implementation/Data/Planning Data/master_planning_roadnet.xlsx',
+#                      sheetName='roadnet_gantt', header=TRUE)
+# 
+# planning$Start = as.POSIXct(planning$Start, d_format)
+# planning$End = as.POSIXct(planning$End, d_format)
+# 
+# months = seq(as.Date("2016/03/01", "%Y/%m/%d"), by="month", length.out=6)
+# monthslab = format(months, format="%b")
+# 
+# vgridpos = as.POSIXct(months, format=d_format)
+# vgridlab = monthslab
+# 
+# 
+# gantt_info = list(labels = planning$Description,
+#                   starts = planning$Start, 
+#                   ends = planning$End,
+#                   format = '%Y-%m-%d', 
+#                   priorities = planning$Priority)
+# 
+# colors = colorRampPalette(c('lightgreen', 'blue'))
+# 
+# time_frame = as.POSIXct(c('2016/3/10', '2016/9/5'), 
+#                         format=d_format)
+# 
+# gantt.chart(gantt_info, 
+#             taskcolors=colors(2), 
+#             xlim = time_frame, 
+#             main = 'Roadnet Timeline',
+#             priority.legend = TRUE,
+#             hgrid = TRUE,
+#             # vgridlab = FALSE,
+#             # vgridlab=vgridlab,
+#             vgrid.format="%b",
+#             border.col = 'black',
+#             xlab = '')  
+#             #,
+#             #vgrid=TRUE)
+#             #vgridpos=vgridpos,vgridlab=vgridlab,hgrid=TRUE)
+# 
 
 
 
@@ -135,6 +410,14 @@ edges = get_edges(graph_nodes_edges,
 # edges = read.csv('C:/Users/pmwash/Desktop/Roadnet Implementation/Data/Planning Data/edges.csv', header=TRUE)
 # nodes = read.csv('C:/Users/pmwash/Desktop/Roadnet Implementation/Data/Planning Data/nodes_w_types.csv', header=TRUE)
 # 
+
+
+
+
+
+
+
+
 
 
 
@@ -374,7 +657,7 @@ graph %>% render_graph()
 
 
 mermaid("
-graph TD
+        graph TD
         a[gamma]-->c(normal1)
         b[omega2]-->c(normal1)
         c-->d[lambda]
