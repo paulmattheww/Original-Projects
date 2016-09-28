0643d354112913fe1bbd1cb89f372fdd432b75d0
Paul Matthew Washburn <paulmattheww@gmail.com>
2016-09-28 11:03:52 -0500

    Update Off_Day_Deliveries_V5.py

--- Professional/Alcoholic-Beverage-Distribution/Reports/Off-Day-Deliveries/Off_Day_Deliveries_V5.py
+++ Professional/Alcoholic-Beverage-Distribution/Reports/Off-Day-Deliveries/Off_Day_Deliveries_V5.py
@@ -7,18 +7,20 @@ from pandas import Series, DataFrame, read_csv
 import numpy as np
 import pandas as pd
 from datetime import datetime as dt
+import datetime
 
 def clean_pw_offday(pw_offday, weeklookup):
     '''
     Clean pw_offday query without filtering out non-off-days
     '''
 deliveries = pw_offday
-    
+
 def as400_date(dat):
     '''Accepts date as formatted in AS400'''
     dat = str(dat)
     dat = dat[-6:]
-    dat = dt.strptime(dat, '%y%m%d')
+    dat = str(dt.date(dt.strptime(dat, '%y%m%d')))
+    return dat
     
 deliveries.columns = ['Date', 'Division', 'Invoice', 'CustomerID', 'Call', 'Priority', 
            'Warehouse', 'Cases', 'Dollars', 'Ship', 'Salesperson', 
@@ -32,14 +34,28 @@ typ_map = {'A':'Bar/Tavern','C':'Country Club','E':'Transportation/Airline','G':
             'Y':'Convenience Store','Z':'Catering','3':'Night Club','5':'Adult Entertainment','6':'Sports Bar',\
             'I':'Church','F':'Membership Club','B':'Mass Merchandiser','H':'Fraternal Organization',\
             '7':'Sports Venue'}
-deliveries.CustomerType = deliveries.CustomerType.map(typ_map)    
-          
-deliveries.Date = [as400_date(deliveries.Date.astype(str)) for d in deliveries.Date.astype(str).tolist()]       
+deliveries.CustomerType = deliveries.CustomerType.astype(str).map(typ_map)    
+
+whs_map = {1:'Kansas City',2:'Saint Louis',3:'Columbia',4:'Cape Girardeau', 5:'Springfield'}
+deliveries.Warehouse = deliveries.Warehouse.map(whs_map)          
+
+deliveries.Date = [as400_date(d) for d in deliveries.Date.astype(str).tolist()]    
+deliveries = deliveries.merge(weeklookup, on='Date')
+
+dat = Series(deliveries.Date.astype(str).tolist())
+deliveries['Weekday'] = Series([dt.strftime(dt.strptime(d, '%Y-%m-%d'), '%A') for d in dat])
+
+week_plan = deliveries.ShipWeekPlan
+week_ship = deliveries.Ship
+month = dat.dt.month
+
+
+deliveries.head(1000)
 
 
 
 pw_offday = read_csv('C:/Users/pmwash/Desktop/R_files/Data Input/Input Files for Reports/Off-Day Deliveries/pw_offday.csv')
-weeklookup = read_csv('N:/Operations Intelligence/Monthly Reports/Data/Reporting/Transfer Files/pw_offday_weeklookup.csv')
+weeklookup = read_csv('N:/Operations Intelligence/Monthly Reports/Data/Reporting/Transfer Files/pw_offday_weeklookup.csv')#change htese paths
 
 
 
