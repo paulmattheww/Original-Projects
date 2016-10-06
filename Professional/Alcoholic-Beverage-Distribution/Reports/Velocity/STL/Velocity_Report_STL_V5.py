0d0657e6eeb4c8b8e9b6869c8480afee996bd430
Paul Matthew Washburn <paulmattheww@gmail.com>
2016-10-06 11:15:38 -0500

    Update Velocity_Report_STL_V5.py

--- Professional/Alcoholic-Beverage-Distribution/Reports/Velocity/STL/Velocity_Report_STL_V5.py
+++ Professional/Alcoholic-Beverage-Distribution/Reports/Velocity/STL/Velocity_Report_STL_V5.py
@@ -1,139 +1,254 @@
 '''
-Off Day and Additional Deliveries
-Re-engineered September/October 2016
+Velocity Report, STL
+Re-Done September 2016
+Ensure saved as Excel document UTF8
+Do NOT pre-sort the data
+Save to same place each time
+Read in the xlsx file exported from Compleo
+Separate out cases and bottles
+Run velocity separation process
+Print to file
+Send to group
 '''
+print('Did you change the month?')
+report_month = 'September 2016'
 
-from pandas import Series, DataFrame, read_csv
-import numpy as np
 import pandas as pd
-from datetime import datetime as dt
-import itertools
+from pandas import read_excel, Series, DataFrame
+import numpy as np
+import re
+
+raw = read_excel('C:/Users/pmwash/Desktop/Re-Engineered Reports/Velocity/Data/velocity_stl.xlsx',header=0)
+
+ttl_cs = 266760
+ttl_btl = 10266
 
 
-
-
-pw_offday = read_csv('C:/Users/pmwash/Desktop/R_files/Data Input/Input Files for Reports/Off-Day Deliveries/pw_offday.csv')
-weeklookup = read_csv('C:/Users/pmwash/Desktop/Re-Engineered Reports/Off Day Deliveries/pw_offday_weeklookup.csv')#change htese paths
-
-
-
-def clean_pw_offday(pw_offday, weeklookup):
-    '''
-    Clean pw_offday query without filtering out non-off-days
-    '''
-deliveries = pw_offday
-
-def as400_date(dat):
-    '''Accepts date as formatted in AS400'''
-    dat = str(dat)
-    dat = dat[-6:]
-    dat = str(dt.date(dt.strptime(dat, '%y%m%d')))
-    return dat
+def pre_process_stl(raw):
+    '''Accepts a .xls output from Compleo unformatted'''
+    # Remove whitespace from column names
+    raw.columns = [x.strip().replace(' ','') for x in raw.columns]
     
-deliveries.columns = ['Date', 'Division', 'Invoice', 'CustomerId', 'Call', 'Priority', 
-           'Warehouse', 'Cases', 'Dollars', 'Ship', 'Salesperson', 
-           'ShipWeekPlan', 'Merchandising', 'OnPremise', 
-           'CustomerSetup', 'CustomerType', 'Customer']
-
-typ = deliveries.CustomerType
-typ_map = {'A':'Bar/Tavern','C':'Country Club','E':'Transportation/Airline','G':'Gambling',\
-            'J':'Hotel/Motel','L':'Restaurant','M':'Military','N':'Fine Dining','O':'Internal',\
-            'P':'Country/Western','S':'Package Store','T':'Supermarket/Grocery','V':'Drug Store',\
-            'Y':'Convenience Store','Z':'Catering','3':'Night Club','5':'Adult Entertainment','6':'Sports Bar',\
-            'I':'Church','F':'Membership Club','B':'Mass Merchandiser','H':'Fraternal Organization',\
-            '7':'Sports Venue'}
-deliveries.CustomerType = deliveries.CustomerType.astype(str).map(typ_map)    
-
-whs_map = {1:'Kansas City',2:'Saint Louis',3:'Columbia',4:'Cape Girardeau', 5:'Springfield'}
-deliveries.Warehouse = deliveries.Warehouse.map(whs_map)          
-
-deliveries.Date = [as400_date(d) for d in deliveries.Date.astype(str).tolist()]    
-deliveries = deliveries.merge(weeklookup, on='Date')
-
-dat = Series(deliveries.Date.tolist())
-dat_f = Series([dt.strptime(d, '%Y-%m-%d') for d in dat])
-deliveries['Weekday'] = Series([dt.strftime(d, '%A') for d in dat_f])
-
-week_plan = deliveries.ShipWeekPlan.tolist()
-week_shipped = deliveries.ShipWeek.tolist()
-
-month = deliveries.Month
-year = deliveries.Year
-
-deliveries.Ship = del_days = [str('%07d'% int(str(day).zfill(0))) for day in deliveries.Ship.astype(str).tolist()]
-
-mon = Series([d[-7:][:1] for d in del_days]).map({'1':'M','0':'_'})
-tue = Series([d[-6:][:1] for d in del_days]).map({'1':'T','0':'_'})
-wed = Series([d[-5:][:1] for d in del_days]).map({'1':'W','0':'_'})
-thu = Series([d[-4:][:1] for d in del_days]).map({'1':'R','0':'_'})
-fri = Series([d[-3:][:1] for d in del_days]).map({'1':'F','0':'_'})
-sat = Series([d[-2:][:1] for d in del_days]).map({'1':'S','0':'_'})
-sun = Series([d[-1:][:1] for d in del_days]).map({'1':'U','0':'_'})
-
-deliveries['DeliveryDays'] = del_days = list(itertools.chain.from_iterable([mon + tue + wed + thu + fri + sat + sun]))
-
-weekday = deliveries.Weekday = [d[:3] for d in deliveries.Weekday.astype(str).tolist()]
-_days = DataFrame(data={'Weekday':weekday, 'WeekPlanned':week_plan, 'WeekShipped':week_shipped, 'DelDays':del_days}) #'Monday':mon, 'Tuesday':tue, 'Wednesday':wed, 'Thursday':thu, 'Friday':fri, 'Saturday':sat, 'Sunday':sun,
-day_list = _days['WeekPlanned'].tolist()
-_days['WeekPlanned'] = [d if d in ['A','B'] else '' for d in day_list]
-
-_week_actual = _days.WeekShipped.tolist()
-_week_plan = _days['WeekPlanned'] = [ship_week if plan_week == '' else plan_week for ship_week, plan_week in zip(_week_actual,_days.WeekPlanned.tolist())]
-_days['OffWeek'] = _off_week = [p != a for p, a in zip(_week_plan, _week_actual)]
-
-off_mon = [str('M' not in d and w == 'Mon')[:1] for d, w in zip(del_days, weekday)]
-off_tue = [str('T' not in d and w == 'Tue')[:1] for d, w in zip(del_days, weekday)]
-off_wed = [str('W' not in d and w == 'Wed')[:1] for d, w in zip(del_days, weekday)]
-off_thu = [str('R' not in d and w == 'Thu')[:1] for d, w in zip(del_days, weekday)]
-off_fri = [str('F' not in d and w == 'Fri')[:1] for d, w in zip(del_days, weekday)]
-off_sat = [str('S' not in d and w == 'Sat')[:1] for d, w in zip(del_days, weekday)]
-off_sun = [str('U' not in d and w == 'Sun')[:1] for d, w in zip(del_days, weekday)]
-
-_off_days = DataFrame({'Mon':off_mon, 'Tue':off_tue, 'Wed':off_wed, 'Thu':off_thu, 
-                       'Fri':off_fri, 'Sat':off_sat, 'Sun':off_sun, 'OffWeek':_off_week, 'Weekday':weekday})
-_off_days = _off_days[['Mon','Tue','Wed','Thu','Fri','Sat','Sun','Weekday','OffWeek']]                           
-_off_days['OffDayDelivery'] = (_off_days['Mon'] == 'T') | (_off_days['Tue'] == 'T') | (_off_days['Wed'] == 'T') | (_off_days['Thu'] == 'T') | (_off_days['Fri'] == 'T') | (_off_days['Sat'] == 'T') | (_off_days['Sun'] == 'T') | (_off_days['OffWeek'] == True)                
-                       
-check_later = _off_days[_off_days['OffDayDelivery'] == True]
-
-deliveries = pd.concat([deliveries,_off_days[['OffWeek','OffDayDelivery']]], axis=1)
-deliveries.Call = deliveries.Call.map({1:'Customer Call', 2:'ROE/EDI', 3:'Salesperson Call', 4:'Telesales'})
-
-setup_date = deliveries.CustomerSetup.astype(str).tolist()
-setup_month = Series([d.zfill(4)[:2] for d in setup_date])
-this_century = [int(d[-2:]) < 20 for d in setup_date]
-setup_year = Series(["20" + s[-2:] if int(s[-2:]) < 20 else "19" + s[-2:] for s in setup_date])
-
-deliveries['CustomerSetup'] = [str(mon) + '-' + str(yr) for mon, yr in zip(setup_month, setup_year)]
-
-last_month = str(dt.now().month - 1).zfill(2)
-this_year = str(dt.now().year)
-m_y_cutoff = last_month + '-' + this_year
-
-transaction_month = [d[5:7] for d in deliveries.Date.tolist()]
-transaction_year = [d[:4] for d in deliveries.Date.tolist()]
-m_y_transaction = [m + '-' + y for m, y in zip(transaction_month, transaction_year)]
-
-deliveries.NewCustomer = ['Y' if m_y_cutoff == transaction else 'N' for transaction in m_y_transaction]
+    # Locate first row where the dataframe begins referring to cases 
+    find_btl_split = Series(raw['SIZE'].astype(str).tolist())
+    find_btl_split = Series([x.strip().replace(' ','') for x in find_btl_split])
     
-agg_funcs = {'OffDayDelivery' : {'CountOffDayDeliveries':sum, 'CountDeliveries':len}, 
-             'Dollars' : {'SumDollars':sum, 'AverageDollars':np.mean}, 
-             'Cases' : {'SumCases':sum, 'AverageCases':np.mean}}
+    find_case_split = Series(raw['BOTTLESALES'].astype(str).tolist())
+    find_case_split = Series([x.strip().replace(' ','') for x in find_case_split])
+    
+    
+    btl_end_ix = min(find_btl_split[find_btl_split == 'OTAL'].index)
+    case_start_ix = min(find_case_split[find_case_split == 'CASESALES'].index)
+    
+    # Split out bottles and cases by finding first case instance
+    btls = raw.loc[0:btl_end_ix-1]
+    cases = raw.loc[case_start_ix+2:].reset_index(drop=True)
+    
+    # Format columns for cases 
+    ##case_cols = cases.iloc[0].astype(str).reset_index(drop=True).tolist()
+    cases.columns = ['PRODUCT#', 'SIZE', 'ANDDESCRIPTION', 'CASESALES', 'PICKFREQUENCY', 'CSE.LOC.',
+           'BTL.LOC.', 'BULK1', 'BOTTLESONHAND']
+    
+    # Remove invalid data from cases and btls
+     # Remove invalid data from cases and btls
+    remove_rows_cases = cases['PRODUCT#'].astype(str).apply(lambda x: str.isnumeric(x) )
+    cases = cases[remove_rows_cases == True]
+    
+    remove_rows_btls = btls['PRODUCT#'].astype(str).apply(lambda x: str.isnumeric(x) )
+    btls = btls[remove_rows_btls == True]
+#    remove_rows_cases = cases['SIZE'].astype(str).apply(lambda x: x not in ['OTAL','REPORT','SIZE',''])
+#    cases = cases[remove_rows_cases == True]
+#    
+#    remove_rows_btls = (btls['SIZE'].astype(str).apply(lambda x: x not in ['OTAL','REPORT','SIZE',''])) | ()
+#    btls = btls[remove_rows_btls == True]
+    
+    
+    def replace_last(source_string, replace_what, replace_with):
+        '''Replaces last occurrence of replace_what'''
+        head, sep, tail = source_string.rpartition(replace_what)
+        return head + replace_with + tail
+    
+    
+    btl_sales = btls['BOTTLESALES'].astype(str).tolist()
+    btl_sales = [b.strip().replace(' ','') for b in btl_sales]
+    btl_sales = [b.strip().replace(',','') for b in btl_sales]
+    btl_sales = ['-' + replace_last(b,'-','')  if b.endswith('-') == True else b for b in btl_sales]
+    btl_sales = Series(btl_sales)
+    btls['BOTTLESALES'] = btl_sales.astype(np.float64)
+    
+    case_sales = cases['CASESALES'].astype(str).tolist()
+    case_sales = [c.strip().replace(' ','') for c in case_sales]
+    case_sales = [c.strip().replace(',','') for c in case_sales]
+    case_sales = ['-' + replace_last(c,'-','')  if c.endswith('-') == True else c for c in case_sales]
+    case_sales = Series(case_sales)
+    cases['CASESALES'] = case_sales.astype(np.float64)
+    
+    check_btls = np.sum(btls['BOTTLESALES'])
+    check_cses = np.sum(cases['CASESALES'])
+    
+    cases.columns = ['PRODUCT#','SIZE','DESCRIPTION','CASESALES','PICKFREQUENCY','CSE.LOC.','BTL.LOC.','BULK1','BOTTLESONHAND']
+    btls.columns = ['PRODUCT#','SIZE','DESCRIPTION','BOTTLESALES','PICKFREQUENCY','CSE.LOC.','BTL.LOC.','BULK1','BOTTLESONHAND']
+    
+    return btls, cases
 
-DataFrame(deliveries.groupby(['CustomerId','Customer','Week']).agg(agg_funcs)).reset_index(drop=True)
-
-#deliveries[deliveries['OffDayDelivery'] == True]
-
-    return deliveries
 
 
-clean_data = clean_pw_offday(pw_offday, weeklookup)
-
-clean_data.head()
+BTLS,CASES = pre_process_stl(raw)
+print('\n\n\nBOTTLES HEADER \n\n\n',BTLS.head(),'\n\n\nCASES HEADER \n',CASES.head())
 
 
 
 
+def map_stl_lines(BTLS,CASES):
+    '''Map STL Lines to locations'''
+    CASES['CASELINE'] = CASES['CSE.LOC.'].astype(str).str[:1]
+    c_lines = list()
+    
+    for i, c in enumerate(CASES['CASELINE']):
+        if c == 'C':
+            c_lines.append('C-Line')
+        elif c == 'D':
+            c_lines.append('D-Line')
+        elif c == 'E':
+            c_lines.append('E-Line')
+        elif c == 'F':
+            c_lines.append('F-Line')
+        elif c == 'G':
+            c_lines.append('G-Line')
+        elif c == 'K':
+            c_lines.append('KegRoom')
+        elif c == 'W':
+            c_lines.append('WineRoom')
+        else:
+            c_lines.append('OddBall-C')
+    
+    CASES['CASELINE'] = c_lines
+    
+    btl_line_indicator = Series(BTLS['BTL.LOC.'].astype(str).str[:1].tolist())
+    bottle_lines = []
+    
+    for i, b in enumerate(btl_line_indicator):
+        if b == 'A':
+            bottle_lines.append('A Line')
+        elif b == 'B':
+            bottle_lines.append('B Line')
+        else:
+            bottle_lines.append('OddBall-B')
+    
+    BTLS['BOTTLELINE'] = bottle_lines
+
+    return CASES, BTLS
+
+
+
+CASES,BTLS = map_stl_lines(BTLS,CASES)
+
+
+
+
+def extract_features(CASES, BTLS, production_days=18):
+    '''Extracts features from the data'''
+    
+    CASES['CASE.SALES.PER.DAY'] = round(CASES.CASESALES / production_days,4)
+    BTLS['BTL.SALES.PER.DAY'] = round(BTLS.BOTTLESALES / production_days,4)
+    
+    return CASES, BTLS
+
+
+
+CASES, BTLS = extract_features(CASES, BTLS)
+
+
+
+
+def create_summary(CASES, BTLS, production_days=18):
+    '''Summarize case lines for STL'''
+    cs_count_items_on_line = CASES[['CASELINE','CASESALES']].groupby('CASELINE').count()
+    cs_count_items_on_line.columns = ['N_SKUS']
+    cs_volume_on_line = CASES[['CASELINE','CASESALES']].groupby('CASELINE').sum()
+    cs_volume_on_line.columns = ['CASE_VOLUME']
+    
+    cs_summary = cs_count_items_on_line.join(cs_volume_on_line)
+    cs_summary['VOLUME_PER_SKU'] = round(cs_summary.CASE_VOLUME / cs_summary.N_SKUS, 2)
+    cs_summary['VOLUME_PER_DAY'] = round(cs_summary.CASE_VOLUME / production_days, 0)
+    total_case_volume = cs_summary['CASE_VOLUME'].sum()
+    cs_summary['PERCENT_TOTAL_VOLUME'] = round(cs_summary.CASE_VOLUME / total_case_volume, 4)
+    
+    btl_count_items_on_line = BTLS[['BOTTLELINE','BOTTLESALES']].groupby('BOTTLELINE').count()
+    btl_count_items_on_line.columns = ['N_SKUS']
+    btl_volume_on_line = BTLS[['BOTTLELINE','BOTTLESALES']].groupby('BOTTLELINE').sum()
+    btl_volume_on_line.columns = ['BOTTLE_VOLUME']
+    
+    btl_summary = btl_count_items_on_line.join(btl_volume_on_line)
+    btl_summary['VOLUME_PER_SKU'] = round(btl_summary.BOTTLE_VOLUME / btl_summary.N_SKUS, 2)
+    btl_summary['VOLUME_PER_DAY'] = round(btl_summary.BOTTLE_VOLUME / production_days, 0)
+    total_bottle_volume = btl_summary['BOTTLE_VOLUME'].sum()
+    btl_summary['PERCENT_TOTAL_VOLUME'] = round(btl_summary.BOTTLE_VOLUME / total_bottle_volume, 4)
+    
+    return cs_summary, btl_summary
+
+
+
+
+def write_stl_to_xlsx(CASES, BTLS, month):
+    '''Write the output to file'''
+    file_out = pd.ExcelWriter('N:/Operations Intelligence/Monthly Reports/Velocity/Velocity-'+month+'.xlsx', engine='xlsxwriter')
+    workbook = file_out.book
+    
+    cs_summary, btl_summary = create_summary(CASES, BTLS)
+    cs_summary.to_excel(file_out, sheet_name='Summary', index=True)
+    btl_summary.to_excel(file_out, sheet_name='Summary', startrow=12, index=True)
+    
+    summary_tab = file_out.sheets['Summary']
+    summary_tab.set_column('A:A',11)
+    summary_tab.set_column('B:B',9)
+    summary_tab.set_column('C:C',16)
+    summary_tab.set_column('D:E',19)
+    summary_tab.set_column('F:F',24)
+    
+    cs_lines = ['C-Line','D-Line','E-Line','F-Line','G-Line','OddBall-C','WineRoom']
+    btl_lines = ['A Line','B Line','OddBall-B']
+    
+    for i, line in enumerate(cs_lines):
+        new_df = DataFrame(CASES[CASES.CASELINE == line])
+        
+        new_df.reset_index(drop=True, inplace=True)
+        print('Inputting %s as its own tab in the velocity report.' % line)
+        tab_name = str(line)
+        new_df.to_excel(file_out, sheet_name=tab_name,index=False)
+        sheet = file_out.sheets[tab_name]
+        sheet.set_column('A:B',10)
+        sheet.set_column('C:C',48)
+        sheet.set_column('D:D',12)
+        sheet.set_column('E:E',15.2)
+        sheet.set_column('F:H',8.5)
+        sheet.set_column('I:I',16)
+        sheet.set_column('J:J',9)
+        sheet.set_column('K:K',19)
+        
+        
+    for i, line in enumerate(btl_lines):
+        new_df = DataFrame(BTLS[BTLS.BOTTLELINE == line])
+        
+        new_df.reset_index(drop=True, inplace=True)
+        print('Inputting %s as its own tab in the velocity report.' % line)
+        tab_name = str(line)
+        new_df.to_excel(file_out, sheet_name=tab_name,index=False)
+        sheet = file_out.sheets[tab_name]
+        sheet.set_column('A:B',10)
+        sheet.set_column('C:C',48)
+        sheet.set_column('D:D',12)
+        sheet.set_column('E:E',15.2)
+        sheet.set_column('F:H',8.5)
+        sheet.set_column('I:I',16)
+        sheet.set_column('J:J',9)
+        sheet.set_column('K:K',19)    
+    
+    file_out.save()
+    
+
+write_stl_to_xlsx(CASES, BTLS, month=report_month)
+  
 
 
 
@@ -156,5 +271,21 @@ clean_data.head()
 
 
 
+
+
+
+
+
+
+
+# all_sizes = raw['SIZEANDDESCRIPTION'].tolist()
+# keg_sizes = ["\\<1/6BL\\>","\\<1/2BL\\>","\\<1/4BL\\>","\\<20L\\>","\\<10.8G\\>","\\<15.5G\\>","\\<15L\\>",
+# "\\<2.6G\\>","\\<19L\\>","\\<3.3G\\>","\\<4.9G\\>","\\<5.16G\\>","\\<5.2G\\>","\\<5.4G\\>","\\<19.5L\\>",
+# "\\<50L\\>","\\<30L\\>","\\<5G\\>","\\<25L\\>"]
+# keg_sizes = '(' + ')|('.join(keg_sizes) + ')'
+
+# [re.match(k,a) for k,a in zip(keg_sizes,all_sizes)]
+
+# re.match(keg_sizes,'1/6BL')
 
 
