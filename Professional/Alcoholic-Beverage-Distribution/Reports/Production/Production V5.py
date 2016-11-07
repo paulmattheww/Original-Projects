41c0535d7ecdf1906aeaa11a558daaceb6efd5cf
Paul Matthew Washburn <paulmattheww@gmail.com>
2016-11-07 14:13:53 -0600

    Update Production V5.py

--- Professional/Alcoholic-Beverage-Distribution/Reports/Production/Production V5.py
+++ Professional/Alcoholic-Beverage-Distribution/Reports/Production/Production V5.py
@@ -18,9 +18,13 @@ pd.set_option('display.max_columns', 100)
 pd.set_option('display.width', 200)
 
 
-print('Defining functions for utility.')
-def extract_stl_production_tab(file, df):
-        '''Gets date from filename'''
+temp_location = 'C:\\Users\\pmwash\\Desktop\\Disposable Docs\\Production Data\\'
+file_list = glob.glob(temp_location + '*.xls*')
+
+
+
+def extract_date_stl(file):
+        '''Takes date from file name'''
         regex_criteria = re.compile(r'[0-9]+-[0-9]+')    
         dat = re.findall(regex_criteria, file)
         
@@ -29,62 +33,114 @@ def extract_stl_production_tab(file, df):
         
         this_year = str(dt.today().year)
         dat = str(dat + '-' + this_year)
-        dat = str(dt.strptime(str(dat), "%m-%d-%Y").date())
+        dat = dt.strptime(str(dat), "%m-%d-%Y").date()
+        return dat
+
+
+def extract_stl_production_tab(file, df):
+        '''
+        Takes in and formats Production Tab from Daily Report. 
+        Extracts date from filename and creates index.
+        Puts into a dictionary of dataframes 
+        for input into a pandas DataFrame.
+        '''
+        dtypes = {'Date':dt.date, 'Warehouse':str,'LOC':str,'RTE':str,'Driver':str,'Truck#':str,
+                'Stops':np.float64,'TTL Cs/splt':np.float64,'Cs':np.float64,'Btls':np.float64,
+                'Start Hr':str, 'End Hr':str,'Ttl Hrs':str,'Ttl Mi':np.float64 }
+        df = pd.read_excel(file, sheetname='Production', converters=dtypes)
+    
+        dat = extract_date_stl(file)
         
-        df['Date'] = dat
-        df.reset_index(drop=True, inplace=True)
-        
+        df['Date'] = dat 
+        df['Month'] = dat.strftime('%B')
+        df['Weekday'] = dat.strftime('%A')
+        df['WeekNumber'] = dat.strftime('%U')
+        df['DOTM'] = dat.strftime('%d')
         df['Warehouse'] = 'STL'
-        
-        df = df[df['Driver'] != 'Totals:']        
         
         keep_cols = ['Date','Warehouse','LOC','RTE','Driver','Truck#','Stops',
                      'TTL Cs/splt','Cs','Btls','Start Hr',
-                     'End Hr','Ttl Hrs','Ttl Mi']
+                     'End Hr','Ttl Hrs','Ttl Mi','Month','Weekday','WeekNumber',
+                     'DOTM']
         df = df[keep_cols].drop_duplicates()
         
-        DATE, WAREHOUSE, ROUTE, DRIVER, TRUCK, CASES = df.Date.astype(str), df.Warehouse.astype(str), df.RTE.astype(str), df.Driver.astype(str), df['Truck#'].astype(str), df['TTL Cs/splt'].astype(str)
-        new_index = DATE + '_' + WAREHOUSE + '_' + ROUTE + '_' + DRIVER + '_' + TRUCK + '_' + CASES
+        WAREHOUSE, ROUTE = df.Warehouse.astype(str), df.RTE.astype(str)
+        new_index = WAREHOUSE + '_' + ROUTE 
+        
         df.set_index(new_index, inplace=True)
+        
+        df = df[df['Driver'] != 'Totals:']        
+        df = df.sort_values(['Stops','TTL Cs/splt'], ascending=False).reset_index(drop=False)
+        
+        df['Date'] = df['Date'].replace(to_replace='NaN', value='')
+        df = df[df['Date'].isnull() == False]
         
         return df, dat
 
 
-print('Read in files.')
-temp_location = 'C:\\Users\\pmwash\\Desktop\\Disposable Docs\\Production Data\\'
-file_list = glob.glob(temp_location + '*.xls*')
-
-panel_data = {}
+Production_Tab = pd.DataFrame()        
 
 for i, file in enumerate(file_list):
-    df = pd.read_excel(file, sheetname='Production')
-    
     df, dat = extract_stl_production_tab(file, df)
+    Production_Tab = Production_Tab.append(df)
+    print('Adding production tab data for %s' % dat)
+
+
+Production_Tab.head(100)
+cols = ['Stops','TTL Cs/splt', 'Ttl Mi']
+Production_Tab.groupby(['LOC'])[cols].sum()
+
+
+
+
+
+def extract_stl_night_hours_tab(file):
+        '''
+        Takes in and formats Night Hours tab from Daily Report. 
+        Extracts date from filename and creates index.
+        Puts into a dictionary of dataframes 
+        for input into a pandas DataFrame.
+        '''
+        senior = pd.read_excel(file, sheetname='Night hours', skiprows=6, skip_footer=1, na_values=['NaN',np.nan,np.NaN,np.NAN], header=0)
+        casual = pd.read_excel(file, sheetname='Night hours', skiprows=34, skip_footer=1, na_values=['NaN',np.nan,np.NaN,np.NAN], header=0)
         
-    panel_data[dat] = df
-    print(dat)
+        dat = extract_date_stl(file)
+        
+        senior['Date'] = casual['Date'] = dat 
+        senior['Month'] = casual['Month']  = dat.strftime('%B')
+        senior['Weekday'] = casual['Weekday']  = dat.strftime('%A')
+        senior['WeekNumber'] = casual['WeekNumber'] = dat.strftime('%U')
+        senior['DOTM'] = casual['DOTM'] = dat.strftime('%d')
+        senior['Warehouse'] = casual['Warehouse'] = 'STL'
+        
+        keep_cols = ['Date','NAME','HRS WORKED','REG TIME','O.T HOURS',
+                    'Month','Weekday','WeekNumber','DOTM','Warehouse']
+            
+        senior = senior[keep_cols].drop_duplicates()
+        casual = casual[keep_cols].drop_duplicates()
+        
+        senior['Type'] = 'Senior'
+        casual['Type'] = 'Casual'
+        
+        senior = senior[senior['NAME'].isnull() == False]
+        casual = casual[casual['NAME'].isnull() == False]
+        
+        df = senior.append(casual)
+        df.reset_index(drop=True, inplace=True)
+        
+        return df
+        
+        
+        
+NightlyHours_Tab = pd.DataFrame()        
 
-
-panel_data['2016-10-03'].columns
-
-panel_data['2016-10-03'].tail()
-
-Production_Tab = pd.Panel(data=panel_data)
-
-
-Production_Tab
-
-
-
-
-
-
-
-#test
-regex_criteria = re.compile(r'[0-9]+-[0-9]+')
 for i, file in enumerate(file_list):
-    dat = re.findall(regex_criteria, file)
-    dt.strptime(str(dat), "['%m-%d']")
+    df = extract_stl_night_hours_tab(file)
+    
+    NightlyHours_Tab = NightlyHours_Tab.append(df)
+
+
+NightlyHours_Tab.head(100)
 
 
 
@@ -101,27 +157,6 @@ for i, file in enumerate(file_list):
 
 
 
-{f: pd.read_excel(f, sheetname='Production') for f in daily_reports}
-
-Production_Tab = pd.Panel()
-
-
-
-{f: pd.read_excel(f, sheetname='Production') for f in daily_reports}
-master_df = DataFrame()
-
-for file in daily_reports:
-    df = pd.read_excel(file, sheetname='Production')
-    df = df[df['RTE'] != np.NaN]
-    master_df = master_df.append(df)
-
-master_df.head(50)
-
-
-
-daily_reports = os.listdir(temp_location)
-
-daily_reports
 
 
 
@@ -130,42 +165,78 @@ daily_reports
 
 
 
-# def move_files_to_neutral_location(): pass
-#kc_base_dir = 'M:/Share/Daily Report KC/2016/October 2016/.'
-#stl_base_dir = 'N:/Daily Report/2016/OCT/.'
-#
-#file_list = os.listdir(stl_base_dir)
-#file_list = [f for f in file_list if f.startswith('~$') == False]
-#destination = 'C:/Users/pmwash/Desktop/Disposable Docs/Production Data/'
-#
-#def copy_daily_reports(file_list, destination, stl_base_dir, kc_base_dir=None):    
-#    
-#    for file in file_list:
-#        if not file.startswith('~'):
-#            file_name = os.path.join(stl_base_dir, file)
-#            print('Copying %s to %s' % (file_name, destination))
-#            shutil.copyfile(file_name, destination)
-#    os.system('cls')
-#    print('Copying complete.')
-#    
-#       
-#
-##os.remove('C:/Users/pmwash/Desktop/Disposable Docs/Production Data/~$08-01.xlsx')
-#
-#copy_daily_reports(file_list, destination, stl_base_dir)
-#os.listdir('C:\\Users\\pmwash\\Desktop\\Disposable Docs\\Production Data')
-#
-#
-#_from = 'N:/Daily Report/2016/SEP'
-#_to = 'C:/Users/pmwash/Desktop/Disposable Docs/Production Data'
-#
-#
-#distutils.dir_util.copy_tree(_from, _to)
-#
-#
-#src = os.path.join('N:/Daily Report/2016/SEP/',file_list[1])
-#shutil.copyfile(src,destination)
-#src
+
+
+
+
+
+
+
+# testing
+extract_stl_night_hours_tab(file_list[1])
+
+
+file = '10-02.xlsx'
+
+senior = pd.read_excel(temp_location + file, sheetname='Night hours', skiprows=6, skip_footer=1, na_values=['NaN',np.nan,np.NaN,np.NAN], header=0)
+casual = pd.read_excel(temp_location + file, sheetname='Night hours', skiprows=34, skip_footer=1, na_values=['NaN',np.nan,np.NaN,np.NAN], header=0)
+
+dat = extract_date_stl(file)
+
+senior['Date'] = casual['Date'] = dat 
+senior['Month'] = casual['Month']  = dat.strftime('%B')
+senior['Weekday'] = casual['Weekday']  = dat.strftime('%A')
+senior['WeekNumber'] = casual['WeekNumber'] = dat.strftime('%U')
+senior['DOTM'] = casual['DOTM'] = dat.strftime('%d')
+senior['Warehouse'] = casual['Warehouse'] = 'STL'
+
+keep_cols = ['Date','NAME','HRS WORKED','REG TIME','O.T HOURS',
+            'Month','Weekday','WeekNumber','DOTM','Warehouse']
+    
+senior = senior[keep_cols].drop_duplicates()
+casual = casual[keep_cols].drop_duplicates()
+
+senior['Type'] = 'Senior'
+casual['Type'] = 'Casual'
+
+senior = senior[senior['NAME'].isnull() == False]
+casual = casual[casual['NAME'].isnull() == False]
+
+df = senior.append(casual)
+df.reset_index(drop=True, inplace=True)
+        
+df.head(50)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Production_Panel = pd.Panel(data=panel_data)
+
+Production_Panel.groupby(axis=1, function=np.sum)
+
+Production_Panel['2016-10-06'].tail(50)
+
+
+Production_Panel['2016-10-03'].columns
+
+
+
+
+
+
 
 
 
