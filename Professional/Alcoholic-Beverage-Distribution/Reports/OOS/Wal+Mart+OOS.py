b199088da18d46a970c8952aa6102daf3176b1ea
Paul Matthew Washburn <paulmattheww@gmail.com>
2017-02-16 15:26:30 -0600

    Update and rename Professional/Alcoholic-Beverage-Distribution/Reports/Wal+Mart+OOS.py to Professional/Alcoholic-Beverage-Distribution/Reports/OOS/Wal+Mart+OOS.py

--- Professional/Alcoholic-Beverage-Distribution/Reports/OOS/Wal+Mart+OOS.py
+++ Professional/Alcoholic-Beverage-Distribution/Reports/OOS/Wal+Mart+OOS.py
@@ -28,11 +28,14 @@ def integrate_walmart_data(path):
     wine = [file for file in all_files if 'Wine' in file]
     
     spirit_ls, spirits_df = [], pd.DataFrame()
+    wine_ls, wine_df = [], pd.DataFrame()
+    
     colnames = ['Store','City','State','Store Type','Buyer','Region','UPC','Item Description','Brand',
                'Size','Supplier','Distributor','Sat','Sun','Mon','Tue','Wed','Thu','Fri','Total OOS',
                'OOS $ Impact']
     no_punct = lambda x: ''.join([i for i in x if i not in string.punctuation])
-
+    
+    ## Process spirits data
     for file in spirits:
         print('Reading in file %s' %file)
         df = pd.read_csv(file, header=0, names=colnames)
@@ -82,8 +85,61 @@ def integrate_walmart_data(path):
         df.reset_index(drop=True, inplace=True)
 
         spirits_df = spirits_df.append(df)
+    
+    ## Process Wine data
+    for file in wine:
+        print('Reading in file %s' %file)
+        df = pd.read_csv(file, header=0, names=colnames)
+        df.Brand = df.Brand.apply(no_punct)
+        df.Store = df.Store.astype(str)
+        df.UPC = df.UPC.astype(str)
         
-    return spirits_df
+        ## Get date from file name
+        get_date = lambda dat: re.split(r'OOS ',re.split(r'.csv', dat)[0])[1]
+        df['Date Sent'] = dat = dt.strptime(get_date(file), '%m%d%Y')
+        df['Weekday Sent'] = format(dat, '%A')
+        
+        ## Clean up inconsistently specified store type data
+        df['Store Type'] = [re.sub('BASE STR', '', s.upper()) for s in df['Store Type'].astype(str).tolist()]
+        df['Store Type'] = [re.sub('-', '', s) for s in df['Store Type'].astype(str).tolist()]
+        df['Store Type'] = [re.sub(' ','',s) for s in df['Store Type']]
+        df['Store Type'] = [re.sub('NGHBRHDMKT','NEIGHBORHOODMARKET',s) for s in df['Store Type']]
+        
+        ## Unstack the weekdays so they are rows not cols
+        identifiers = ['Date Sent','Weekday Sent','Store','City','State','Store Type','Buyer',
+                       'Region','UPC','Item Description','Brand','Size','Supplier','Distributor',
+                       'Total OOS','OOS $ Impact']
+        df.set_index(identifiers, inplace=True)
+        df = pd.DataFrame(df.stack()).reset_index(drop=False)
+        df.rename(columns={'level_16':'Weekday', 0:'OOS'}, inplace=True)
+        
+        ## Get date of observation (not same as file) 
+        wday_sent = int(format(dat, '%w'))
+        df.loc[df.Weekday == 'Mon', 'Date'] = dat - pd.offsets.Day(7)
+        df.loc[df.Weekday == 'Tue', 'Date'] = dat - pd.offsets.Day(6)
+        df.loc[df.Weekday == 'Wed', 'Date'] = dat - pd.offsets.Day(5)
+        df.loc[df.Weekday == 'Thu', 'Date'] = dat - pd.offsets.Day(4)
+        df.loc[df.Weekday == 'Fri', 'Date'] = dat - pd.offsets.Day(3)
+        df.loc[df.Weekday == 'Sat', 'Date'] = dat - pd.offsets.Day(2)
+        df.loc[df.Weekday == 'Sun', 'Date'] = dat - pd.offsets.Day(1)
+        df['Weekday'] = [format(dat, '%A') for dat in df.Date]
+        df['DOTM'] = [format(dat, '%d') for dat in df.Date]
+        df['Week'] = [format(dat, '%U') for dat in df.Date]
+        
+        ## Make it more readable
+        df.drop(['Date Sent','Weekday Sent','Total OOS','OOS $ Impact','Distributor','State'], axis=1, inplace=True)
+        keep_cols = ['Date','Weekday','Week','DOTM','Region','Store','OOS',
+                     'Supplier','Brand','Item Description','Size','UPC','City',
+                     'Store Type']
+        df = df[keep_cols]
+        df.sort_values(['Date','Store'], inplace=True)
+        df.reset_index(drop=True, inplace=True)
+
+        wine_df = wine_df.append(df)
+    
+    all_data = spirits_df.append(wine_df)
+    
+    return all_data
 
 walmart_clean = integrate_walmart_data(path)
 print(walmart_clean.tail())
@@ -99,7 +155,7 @@ def aggregate_walmart_data(walmart_clean):
 print(aggregate_walmart_data(walmart_clean).head())
 
 
-# In[4]:
+# In[6]:
 
 def munge_walmart_dive():
     walmart_dive = pd.read_csv('C:\\Users\\pmwash\\Desktop\\Wal Mart\\Out of Stocks\\Raw Data\\Wal Mart Dive YTD Sales.csv', 
@@ -150,7 +206,7 @@ walmart_receipts = munge_walmart_dive()
 print(walmart_receipts.head())
 
 
-# In[5]:
+# In[7]:
 
 def aggregate_walmart(walmart_receipts):
     ## Extract some knowledge
@@ -182,7 +238,7 @@ agg_walmart_receipts = aggregate_walmart(walmart_receipts)
 print(agg_walmart_receipts.head())
 
 
-# In[6]:
+# In[31]:
 
 def merge_walmart_diver_upc(agg_walmart_receipts, walmart_clean):
     botupc = pd.read_csv('C:\\Users\\pmwash\\Desktop\\Wal Mart\\Out of Stocks\\Raw Data\\botupc.csv', 
@@ -214,10 +270,7 @@ def merge_walmart_diver_upc(agg_walmart_receipts, walmart_clean):
     return combined
 
 MAIN_DF = merge_walmart_diver_upc(agg_walmart_receipts, walmart_clean)
-print(MAIN_DF.tail())
-
-
-# In[7]:
+print(MAIN_DF.tail(),'\n\n\n\n\n\n')
 
 def engineer_features(MAIN_DF):
     '''Uses existing data to enrich the combined dataset'''
@@ -227,9 +280,30 @@ def engineer_features(MAIN_DF):
     MAIN_DF['Avg Days Between Orders - Numeric'] = MAIN_DF['Avg Days Between Orders'].astype(str)
     MAIN_DF['Avg Days Between Orders - Numeric'] = [int(s[:2]) for s in MAIN_DF['Avg Days Between Orders - Numeric']]
     
+    MAIN_DF.drop_duplicates(subset=['Store','UPC'],  inplace=True)
+    
     return MAIN_DF
 
-print(engineer_features(MAIN_DF).head())
+MAIN_DF = engineer_features(MAIN_DF)
+print(MAIN_DF.tail())
+
+
+# In[27]:
+
+def engineer_features(MAIN_DF):
+    '''Uses existing data to enrich the combined dataset'''
+    MAIN_DF['Avg Cases per Order'] = np.divide(MAIN_DF['NonStd Cases'], MAIN_DF['Number of Orders'])
+    MAIN_DF['Avg GP$ per Order'] = np.divide(MAIN_DF['GP $'], MAIN_DF['Number of Orders'])
+    MAIN_DF['Avg OOS per Order'] = np.divide(MAIN_DF['OOS'], MAIN_DF['Number of Orders'])
+    MAIN_DF['Avg Days Between Orders - Numeric'] = MAIN_DF['Avg Days Between Orders'].astype(str)
+    MAIN_DF['Avg Days Between Orders - Numeric'] = [int(s[:2]) for s in MAIN_DF['Avg Days Between Orders - Numeric']]
+    
+    MAIN_DF.drop_duplicates(subset=['Store','UPC','Size'],  inplace=True)
+    
+    return MAIN_DF
+
+MAIN_DF = engineer_features(MAIN_DF)
+print(MAIN_DF.tail())
 
 
 # In[22]:
@@ -260,25 +334,26 @@ pd.DataFrame(MAIN_DF.groupby('Store')['Avg Days Between Orders - Numeric'].mean(
 # In[23]:
 
 #MAIN_DF.hist('OOS', figsize=(12,9), bins=35)
-print(MAIN_DF.head())
+print(MAIN_DF.columns)
 plt.figure(num=None, figsize=(15, 9))
-plt.scatter(MAIN_DF['Avg Days Between Orders - Numeric'], MAIN_DF['Avg Cases per Order'],
-           s=MAIN_DF['Dollars'],
-           c=MAIN_DF['Store'],
-           alpha=.5)
-plt.xlabel('Avg Days betwen Orders @ SKU Level')
-plt.ylabel('Avg Cases per Order @ SKU Level')
+plt.scatter(MAIN_DF['Number of Orders'], 
+            MAIN_DF['OOS'],
+            s=MAIN_DF['Dollars'],
+            c=MAIN_DF['Store'],
+            alpha=.5)
+plt.xlabel('Number of Orders YTD @ SKU/Store Level')
+plt.ylabel('Out of Stocks @ SKU/Store Level')
 plt.show()
 
 
-# In[27]:
+# In[24]:
 
 import seaborn as sns
 print(MAIN_DF.head())
 
 pplot_cols = ['Customer','OOS','Dollars', 'Costs', 'NonStd Cases', 'GP $', 'Mark Up',
-                      'Avg Cases per Order', 'Avg GP$ per Order',
-                      'Avg OOS per Order', 'Avg Days Between Orders - Numeric']
+                'Avg Cases per Order', 'Avg GP$ per Order',
+                'Avg OOS per Order', 'Avg Days Between Orders - Numeric']
 g = sns.pairplot(MAIN_DF[pplot_cols], #hue='Customer', 
              dropna=True, 
              diag_kind='kde',
@@ -286,7 +361,7 @@ g = sns.pairplot(MAIN_DF[pplot_cols], #hue='Customer',
 g.map_upper(sns.residplot)
 
 
-# In[25]:
+# In[ ]:
 
 def generate_weeks():
     DAT = pd.date_range('1/1/2017', periods=366, freq='D')
@@ -307,8 +382,24 @@ def generate_weeks():
 print(generate_weeks())
 
 
-# In[26]:
+# In[32]:
 
-wine = [file for file in all_files if 'Wine' in file]
-print(wine)
+from pandas import ExcelWriter
+
+def save_xlsx(list_dfs, xlsx_path):
+    writer = ExcelWriter(xlsx_path)
+    for n, df in enumerate(list_dfs):
+        df.to_excel(writer, '%s' %n)
+        print('Saving %s' %n)
+    writer.save()
+    print('Finished writing to file')
+    return None
+
+today = dt.today().date(); print(today)
+xlsx_path = 'C:/Users/pmwash/Desktop/Wal Mart/Wal Mart OOS Summary ' + str(today) +'.xlsx' 
+list_dfs = [MAIN_DF, walmart_clean, walmart_receipts]
+
+#save_xlsx(list_dfs, xlsx_path)
+
+MAIN_DF.to_csv('C:/Users/pmwash/Desktop/Wal Mart/Out of Stocks/Raw Data/R/Wal Mart OOS Summary ' + str(today) +'.xlsx' )
 
