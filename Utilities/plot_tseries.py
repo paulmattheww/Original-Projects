2f25b102b43610bdfc39909668663ac42578bd93
Paul Matthew Washburn <paulmattheww@gmail.com>
2018-04-20 15:48:59 -0500

    Update plot_tseries.py

--- Utilities/plot_tseries.py
+++ Utilities/plot_tseries.py
@@ -11,6 +11,8 @@ def plot_tseries_over_group(df, xcol, ycol, grpcol, title_prepend='{}', labs=Non
      - xcol: str of column name in df for datetime series
      - ycol: str of column name in df for tseries 
      - grpcol: str of column name in df of group over which to plot
+     - labs: dict of xlab, ylab
+     - title_prepend: str containing "{}" that prepends group names in title
     '''
     unique_grp_vals = df[grpcol].unique()
     fig, axes = plt.subplots(1, len(unique_group_values), figsize=(17, 6))
@@ -38,12 +40,22 @@ plot_tseries_over_group(stl_daily, xcol, ycol, grpcol, title_prepend, labs, x_an
 
 
 def plot_tseries_histograms_over_group(df, xcol, grpcol, labs=None, title_prepend='{}'):
+    '''
+    Function for plotting histogream of time series df[ycol] over datetime 
+    range df[xcol] using the unique_grp_vals contained in df[grpcol].unique().  
+    
+     - df: pd.DataFrame containing datetime and series to plot
+     - xcol: str of column name in df for datetime series
+     - grpcol: str of column name in df of group over which to plot
+     - labs: dict of xlab, ylab
+     - title_prepend: str containing "{}" that prepends group names in title
+    '''
     unique_grp_vals = df[grpcol].unique()
     fig, axes = plt.subplots(1, 2, figsize=(17, 6))
     for i, grp in enumerate(unique_grp_vals):
         _df = df.loc[df[grpcol] == grp]
         ax = axes[i]
-        ax.hist(_df[xcol], bins=_df['rte'].max()-_df['rte'].min())
+        ax.hist(_df[xcol], bins=_df[xcol].max()-_df[xcol].min())
         ax.axvline(_df[xcol].mean(), linestyle='--', color='r')
         ax.axvline(_df[xcol].mean()-_df[xcol].std(), linestyle='-.', color='y')
         ax.axvline(_df[xcol].mean()+_df[xcol].std(), linestyle='-.', color='y')
