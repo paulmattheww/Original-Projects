1e487c8dfe0edbb0eccea83c064b42676211800a
Paul Matthew Washburn <paulmattheww@gmail.com>
2018-04-21 15:08:05 -0500

    Update plot_tseries.py

--- Utilities/plot_tseries.py
+++ Utilities/plot_tseries.py
@@ -78,3 +78,83 @@ xcol = 'value'
 title_prepend = 'Histogram of Daily Value $USD of Cryptocurrency:  {}'
 labs = dict(xlab='Bins', ylab='Count of Observations')
 plot_tseries_histograms_over_group(df_eda_melt, xcol, grpcol, labs, title_prepend)
+
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+%matplotlib inline
+
+years = mdates.YearLocator()   # every year
+months = mdates.MonthLocator()  # every month
+yearsFmt = mdates.DateFormatter('%Y')
+
+
+def plot_tseries_over_group_with_histograms(df, xcol, ycol, grpcol, title_prepend='{}', 
+                            labs=None, x_angle=0, labelpad=60):
+    '''
+    Function for plotting time series df[ycol] over datetime range df[xcol]
+    using the unique_grp_vals contained in df[grpcol].unique().  
+    
+     - df: pd.DataFrame containing datetime and series to plot
+     - xcol: str of column name in df for datetime series
+     - ycol: str of column name in df for tseries 
+     - grpcol: str of column name in df of group over which to plot
+     - labs: dict of xlab, ylab
+     - title_prepend: str containing "{}" that prepends group names in title
+    '''
+    unique_grp_vals = df[grpcol].unique()
+    figsize = (15, 5 * len(unique_grp_vals))
+    fig, axes = plt.subplots(len(unique_grp_vals), 2, figsize=figsize)
+    j = 0
+    for i, grp in enumerate(unique_grp_vals):
+        _df = df.loc[df[grpcol] == grp]
+        ax = axes[i, j]
+        ax.plot(_df[xcol], _df[ycol])
+        mu, sigma = _df[ycol].mean(), _df[ycol].std()
+        ax.axhline(mu, linestyle='--', color='r')
+        ax.axhline(mu - sigma, linestyle='-.', color='y')
+        ax.axhline(mu + sigma, linestyle='-.', color='y')
+        ax.set_title(title_prepend.format(grp))
+        if labs is not None:
+            ax.set_xlabel(labs['xlab'])
+            ax.set_ylabel(labs['ylab'])
+        ax.xaxis.labelpad = labelpad
+        ax.xaxis.set_major_locator(years)
+        #ax.xaxis.set_major_formatter(yearsFmt)
+        ax.xaxis.set_minor_locator(months)
+        ax.grid(alpha=.1)
+        if x_angle != 0:
+            for tick in ax.get_xticklabels():
+                tick.set_rotation(x_angle)
+        
+        j += 1
+        title_prepend = 'Histogram of ' + str(title_prepend)
+        
+        ax = axes[i, j]
+        ax.hist(_df[ycol], orientation="horizontal")
+        ax.axhline(mu, linestyle='--', color='r')
+        ax.axhline(mu - sigma, linestyle='-.', color='y')
+        ax.axhline(mu + sigma, linestyle='-.', color='y')
+        ax.set_title(title_prepend.format(grp))
+        if labs is not None:
+            ax.set_ylabel('Bins')
+            ax.set_xlabel('Count of Observations')
+        ax.grid(alpha=.1)
+        
+        j -= 1
+                
+    sns.set_style("whitegrid")
+    sns.despine()
+    plt.show()
+    
+title_prepend = 'Daily Value of {}'
+xcol = 'date'
+ycol = 'value'
+grpcol = 'variable'
+labs = dict(xlab='Date', ylab='Value')
+    
+plot_tseries_over_group_with_histograms(df_eda_melt, xcol, ycol, grpcol, title_prepend, labs, x_angle=90)
+
+
