f90330ca47e280623560d527a3ff48c7e46cffe9
Paul Matthew Washburn <paulmattheww@gmail.com>
2018-04-21 13:19:54 -0500

    Update plot_tseries.py

--- Utilities/plot_tseries.py
+++ Utilities/plot_tseries.py
@@ -15,8 +15,8 @@ def plot_tseries_over_group(df, xcol, ycol, grpcol, title_prepend='{}', labs=Non
      - title_prepend: str containing "{}" that prepends group names in title
     '''
     unique_grp_vals = df[grpcol].unique()
-    fig, axes = plt.subplots(1, len(unique_group_values), figsize=(17, 6))
-    for i, grp in enumerate(unique_group_values):
+    fig, axes = plt.subplots(1, len(unique_grp_vals), figsize=(17, 6))
+    for i, grp in enumerate(unique_grp_vals):
         _df = df.loc[df[grpcol] == grp]
         ax = axes[i]
         ax.plot(_df[xcol], _df[ycol])
@@ -30,16 +30,16 @@ def plot_tseries_over_group(df, xcol, ycol, grpcol, title_prepend='{}', labs=Non
                 tick.set_rotation(x_angle)
     plt.show()
     
-title_prepend = 'Routes per Day for {}'
+title_prepend = 'Daily Value of {}'
 xcol = 'date'
-ycol = 'rte'
-grpcol = 'loc'
-labs = dict(xlab='Date', ylab='Number of Market Routes')
+ycol = 'value'
+grpcol = 'variable'
+labs = dict(xlab='Date', ylab='Value')
     
-plot_tseries_over_group(stl_daily, xcol, ycol, grpcol, title_prepend, labs, x_angle=90)
+plot_tseries_over_group(df_eda_melt, xcol, ycol, grpcol, title_prepend, labs, x_angle=90)
 
 
-def plot_tseries_histograms_over_group(df, xcol, grpcol, labs=None, title_prepend='{}'):
+def plot_tseries_histograms_over_group(df, xcol, grpcol, labs=None, title_prepend='{}', resolution_deflator=1):
     '''
     Function for plotting histogream of time series df[ycol] over datetime 
     range df[xcol] using the unique_grp_vals contained in df[grpcol].unique().  
@@ -55,18 +55,18 @@ def plot_tseries_histograms_over_group(df, xcol, grpcol, labs=None, title_prepen
     for i, grp in enumerate(unique_grp_vals):
         _df = df.loc[df[grpcol] == grp]
         ax = axes[i]
-        ax.hist(_df[xcol], bins=_df[xcol].max()-_df[xcol].min())
+        ax.hist(_df[xcol], bins=int((_df[xcol].max()-_df[xcol].min()) / resolution_deflator) )
         ax.axvline(_df[xcol].mean(), linestyle='--', color='r')
         ax.axvline(_df[xcol].mean()-_df[xcol].std(), linestyle='-.', color='y')
         ax.axvline(_df[xcol].mean()+_df[xcol].std(), linestyle='-.', color='y')
-        ax.set_title(title_prepend.format(loc))
+        ax.set_title(title_prepend.format(grp))
         if labs is not None:
             ax.set_xlabel(labs['xlab'])
             ax.set_ylabel(labs['ylab'])
         ax.grid(alpha=.4)
     plt.show()
 
-xcol = 'rte'
-title_prepend = 'Histogram of Routes per Day for {}'
-labs = dict(xlab='Count of Routes per Day', ylab='Number of Observations')
-plot_tseries_histograms_over_group(stl_daily, xcol, grpcol, labs, title_prepend)
+xcol = 'value'
+title_prepend = 'Histogram of Daily Value Observations of {}'
+labs = dict(xlab='Bins', ylab='Count of Observations')
+plot_tseries_histograms_over_group(df_eda_melt, xcol, grpcol, labs, title_prepend)
