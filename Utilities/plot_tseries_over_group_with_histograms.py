3dc8460fa7a5961863c02b139340b06f3f2b83a0
Paul Matthew Washburn <paulmattheww@gmail.com>
2018-04-23 15:40:46 -0500

    Update plot_tseries_over_group_with_histograms.py

--- Utilities/plot_tseries_over_group_with_histograms.py
+++ Utilities/plot_tseries_over_group_with_histograms.py
@@ -10,7 +10,7 @@ yearsFmt = mdates.DateFormatter('%Y')
 
 
 def plot_tseries_over_group_with_histograms(df, xcol, ycol, grpcol, title_prepend='{}', 
-                            labs=None, x_angle=0, labelpad=60, window=10):
+                            labs=None, x_angle=0, labelpad=60, window=15):
     '''
     Function for plotting time series df[ycol] over datetime range df[xcol]
     using the unique_grp_vals contained in df[grpcol].unique().  
@@ -31,21 +31,26 @@ def plot_tseries_over_group_with_histograms(df, xcol, ycol, grpcol, title_prepen
     for i, grp in enumerate(unique_grp_vals):
         _df = df.loc[df[grpcol] == grp]
         ax = axes[i]
-        ax.plot(_df[xcol], _df[ycol], alpha=.5, color='black')
-        ax.plot(_df[xcol], _df[ycol].rolling(window=window).mean(), 
-                alpha=.8, color='r', label='{} period rolling avg'.format(window))
+        ax.plot(_df[xcol], _df[ycol], alpha=.2, color='black')
+        ax.plot(_df[xcol], _df[ycol].rolling(window=window, min_periods=min(5, window)).mean(), 
+                alpha=.5, color='r', label='{} period rolling avg'.format(window),
+                linestyle='--')
+        longer_window = int(window * 3)
+        ax.plot(_df[xcol], _df[ycol].rolling(window=longer_window, min_periods=5).mean(), 
+                alpha=.8, color='darkred', label='{} period rolling avg'.format(longer_window),
+                linewidth=2)
         mu, sigma = _df[ycol].mean(), _df[ycol].std()
         ax.axhline(mu, linestyle='--', color='r', alpha=.3)
         ax.axhline(mu - sigma, linestyle='-.', color='y', alpha=.3)
         ax.axhline(mu + sigma, linestyle='-.', color='y', alpha=.3)
         ax.set_title(title_prepend.format(grp))
         ax.legend(loc='best')
+        bottom, top = mu - 3*sigma, mu + 3*sigma
+        ax.set_ylim((bottom, top))
         if labs is not None:
             ax.set_xlabel(labs['xlab'])
             ax.set_ylabel(labs['ylab'])
         ax.xaxis.labelpad = labelpad
-        #ax.xaxis.set_major_locator(years)
-        #ax.xaxis.set_major_formatter(yearsFmt)
         ax.xaxis.set_minor_locator(months)
         ax.grid(alpha=.1)
         if x_angle != 0:
