37508250f364c52137e0c3f37204ee23d9e1edd2
Paul Matthew Washburn <paulmattheww@gmail.com>
2018-04-23 15:12:33 -0500

    Update plot_tseries_over_group_with_histograms.py

--- Utilities/plot_tseries_over_group_with_histograms.py
+++ Utilities/plot_tseries_over_group_with_histograms.py
@@ -10,7 +10,7 @@ yearsFmt = mdates.DateFormatter('%Y')
 
 
 def plot_tseries_over_group_with_histograms(df, xcol, ycol, grpcol, title_prepend='{}', 
-                            labs=None, x_angle=0, labelpad=60):
+                            labs=None, x_angle=0, labelpad=60, window=10):
     '''
     Function for plotting time series df[ycol] over datetime range df[xcol]
     using the unique_grp_vals contained in df[grpcol].unique().  
@@ -24,24 +24,27 @@ def plot_tseries_over_group_with_histograms(df, xcol, ycol, grpcol, title_prepen
     '''
     unique_grp_vals = df[grpcol].unique()
     nrows = len(unique_grp_vals) // 2
-    figsize = (9, 7 * nrows)
+    figsize = (12, 12 * nrows)
     fig, axes = plt.subplots(len(unique_grp_vals), 1, figsize=figsize)
     title_prepend_hist = 'Histogram of ' + str(title_prepend)
     j = 0
     for i, grp in enumerate(unique_grp_vals):
         _df = df.loc[df[grpcol] == grp]
         ax = axes[i]
-        ax.plot(_df[xcol], _df[ycol], alpha=.7, color='black')
+        ax.plot(_df[xcol], _df[ycol], alpha=.5, color='black')
+        ax.plot(_df[xcol], _df[ycol].rolling(window=window).mean(), 
+                alpha=.8, color='r', label='{} period rolling avg'.format(window))
         mu, sigma = _df[ycol].mean(), _df[ycol].std()
-        ax.axhline(mu, linestyle='--', color='r')
-        ax.axhline(mu - sigma, linestyle='-.', color='y')
-        ax.axhline(mu + sigma, linestyle='-.', color='y')
+        ax.axhline(mu, linestyle='--', color='r', alpha=.3)
+        ax.axhline(mu - sigma, linestyle='-.', color='y', alpha=.3)
+        ax.axhline(mu + sigma, linestyle='-.', color='y', alpha=.3)
         ax.set_title(title_prepend.format(grp))
+        ax.legend(loc='best')
         if labs is not None:
             ax.set_xlabel(labs['xlab'])
             ax.set_ylabel(labs['ylab'])
         ax.xaxis.labelpad = labelpad
-        ax.xaxis.set_major_locator(years)
+        #ax.xaxis.set_major_locator(years)
         #ax.xaxis.set_major_formatter(yearsFmt)
         ax.xaxis.set_minor_locator(months)
         ax.grid(alpha=.1)
@@ -51,11 +54,11 @@ def plot_tseries_over_group_with_histograms(df, xcol, ycol, grpcol, title_prepen
 
         divider = make_axes_locatable(ax)
         axHisty = divider.append_axes('right', 1.2, pad=0.1, sharey=ax)
-        axHisty.grid(alpha=.3)
+        axHisty.grid(alpha=.1)
         axHisty.hist(_df[ycol].dropna(), orientation='horizontal', alpha=.5, color='lightgreen', bins=25)
-        axHisty.axhline(mu, linestyle='--', color='r', label='mu')
-        axHisty.axhline(mu - sigma, linestyle='-.', color='y', label='+/- two sigma')
-        axHisty.axhline(mu + sigma, linestyle='-.', color='y')
+        axHisty.axhline(mu, linestyle='--', color='r', label='mu', alpha=.3)
+        axHisty.axhline(mu - sigma, linestyle='-.', color='y', label='+/- two sigma', alpha=.3)
+        axHisty.axhline(mu + sigma, linestyle='-.', color='y', alpha=.3)
         axHisty.legend(loc='best')
         
         j += 1
@@ -63,12 +66,11 @@ def plot_tseries_over_group_with_histograms(df, xcol, ycol, grpcol, title_prepen
     sns.set_style("whitegrid")
     sns.despine()
     plt.show()
-    
-    
-title_prepend = 'Daily Value of {}'
+
+title_prepend = 'Number of Trucks to Market for {}'
 xcol = 'date'
-ycol = 'value'
-grpcol = 'variable'
-labs = dict(xlab='', ylab='Value')
+ycol = 'rte'
+grpcol = 'loc'
+labs = dict(xlab='', ylab='Number of Trucks')
     
-plot_tseries_over_group_with_histograms(df_eda_melt, xcol, ycol, grpcol, title_prepend, labs, x_angle=90)
+plot_tseries_over_group_with_histograms(stl_daily, xcol, ycol, grpcol, title_prepend, labs, x_angle=90)
