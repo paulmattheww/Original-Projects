cc47a47a21573f4d3937f2890195bf99cd38329d
Paul Matthew Washburn <paulmattheww@gmail.com>
2018-04-24 16:31:27 -0500

    Delete writeTxtSeries.R

--- Professional/Utility_Scripts/writeTxtSeries.R
+++
@@ -1,7 +0,0 @@
-writeTextSeries = function(x){
-  n = length(x)
-  for(i in 1:n){
-    filename = paste0("file_number_",i,".txt")
-    write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
-  }
-}
