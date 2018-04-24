b2d3e5f9d7c32271f3a29bcc2d797786fff40d0c
Paul Matthew Washburn <paulmattheww@gmail.com>
2018-04-24 16:19:01 -0500

    Delete LabelCreation_UPC_Acquisition.sql

--- Professional/Alcoholic-Beverage-Distribution/LabelCreation_UPC_Acquisition.sql
+++
@@ -1,2 +0,0 @@
-select t_item_master.item_number, t_item_master.upc, t_fwd_pick.location_id from t_item_master inner join t_fwd_pick on
-t_item_master.item_number = t_fwd_pick.item_number where ((t_fwd_pick.location_id like 'A%') or (t_fwd_pick.location_id like 'B%' ))
