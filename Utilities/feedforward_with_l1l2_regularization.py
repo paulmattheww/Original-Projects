7675b91c4b98b3dc179d5e137e5a089e10bda971
Paul Matthew Washburn <paulmattheww@gmail.com>
2018-04-25 14:53:46 -0500

    Create feedforward_with_l1l2_regularization.py

---
+++ Utilities/feedforward_with_l1l2_regularization.py
@@ -0,0 +1,38 @@
+from keras.regularizers import *
+
+epochs = 5
+num_neurons = 4
+num_dense_layers = 10
+reg = .000001
+batch_size = 32
+
+model = models.Sequential()
+model.add(layers.Dense(num_neurons, activation='relu', 
+                      input_shape=(X_train.shape[1], ),
+                      kernel_regularizer=l2(reg),
+                      activity_regularizer=l1(reg)))
+
+for lyr in range(num_dense_layers):
+    model.add(layers.Dense(num_neurons, activation='relu',
+                          kernel_regularizer=l2(reg),
+                          activity_regularizer=l1(reg)))
+
+model.add(layers.Dense(1))
+
+model.summary()
+
+model.compile(optimizer='rmsprop', 
+             loss='mean_absolute_percentage_error',
+             metrics=['accuracy'])
+
+history = model.fit(X_train, y_train,
+                   epochs=epochs,
+                   batch_size=batch_size,
+                   verbose=0,
+                   validation_data=(X_val, y_val))
+
+plot_accuracy_loss(history.history)
+pred = model.predict(X_test)
+_r2 = r2_score(y_test, pred)
+
+print('R-squared on test data = %.4f' % _r2)
