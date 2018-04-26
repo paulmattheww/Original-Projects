68434635c95ee0ab2bcf385195c8103db378fd30
Paul Matthew Washburn <paulmattheww@gmail.com>
2018-04-26 16:14:30 -0500

    Create toggle_ipython_code.py

---
+++ Utilities/toggle_ipython_code.py
@@ -0,0 +1,19 @@
+# paul washburn
+# embed this in your jupyter notebook to toggle code
+
+# @hidden_cell
+from IPython.display import HTML
+
+HTML('''<script>
+code_show=true; 
+function code_toggle() {
+ if (code_show){
+ $('div.input').hide();
+ } else {
+ $('div.input').show();
+ }
+ code_show = !code_show
+} 
+$( document ).ready(code_toggle);
+</script>
+<form action="javascript:code_toggle()"><input type="submit" value="Click here to toggle on/off the raw code."></form>''')
