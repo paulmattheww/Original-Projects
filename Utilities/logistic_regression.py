3319bf4bfcf514e450e2b4bd5559722da28ea334
Paul Matthew Washburn <paulmattheww@gmail.com>
2018-04-28 10:52:43 -0500

    Update logistic_regression.py

--- Utilities/logistic_regression.py
+++ Utilities/logistic_regression.py
@@ -3,23 +3,6 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.externals import joblib
 
-y_col = 'schlafly'
-leave_out = ['date', 'stops_per_day', 'routes_per_day'] + [y_col]
-X_cols = [col for col in df_modeling.columns if col not in leave_out]
-continuous_cols = []
-
-X_train, X_test, y_train, y_test, std_scaler = split_scale_data(df_modeling, 
-                                                               y_col, 
-                                                               leave_out, 
-                                                               train_size=.7,
-                                                               continuous_cols=continuous_cols)
-
-# split off hold out set
-pct_hold_out = .5
-nrow_train = int(pct_hold_out * X_test.shape[0])
-X_val, y_val = X_test.iloc[:nrow_train], y_test.iloc[:nrow_train]
-X_test, y_test = X_test.iloc[nrow_train:], y_test.iloc[nrow_train:]
-
 param_grid = {'C': [.0001, .001, .01, .1, 1, 10, 100, 1000, 10000],
              'penalty': ['l1', 'l2']}
 model = LogisticRegression(n_jobs=-1, random_state=7)
