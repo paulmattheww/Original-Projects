08ac29324a2640265892cf5a8675e1328d9863a1
Paul Matthew Washburn <paulmattheww@gmail.com>
2018-05-05 10:04:53 -0500

    Create timing_function_wrapper.py

---
+++ Utilities/timing_function_wrapper.py
@@ -0,0 +1,15 @@
+# paul washburn
+def timing_function(some_function):
+    '''
+    Decorator function.  Outputs the time a function takes to execute.
+    '''
+    @wraps(some_function)
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        result = some_function(*args, **kwargs)
+        t2 = time.time()
+        time_elapsed = round((t2 - t1), 2)
+        print('Runtime: ' + str(time_elapsed) + ' seconds')
+        return result
+    
+    return wrapper
